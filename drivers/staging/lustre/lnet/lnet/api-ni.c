f288d347403bd6026a58a4510f295ced45e828b8
Amir Shehata <amir.shehata@intel.com>
2018-10-05 10:30:07 +1000

    lustre: lnet: rename peer key_nid to prim_nid
    To make the interface clear, renamed key_nid to
    prim_nid to indicate that this parameter refers to
    the peer's primary nid.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-7734
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Reviewed-on: http://review.whamcloud.com/23460
    Reviewed-by: Olaf Weber <olaf@sgi.com>
    Reviewed-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/staging/lustre/lnet/lnet/api-ni.c
+++ drivers/staging/lustre/lnet/lnet/api-ni.c
@@ -2728,7 +2728,7 @@ LNetCtl(unsigned int cmd, void *arg)
 			return -EINVAL;
 
 		mutex_lock(&the_lnet.ln_api_mutex);
-		rc = lnet_add_peer_ni_to_peer(cfg->prcfg_key_nid,
+		rc = lnet_add_peer_ni_to_peer(cfg->prcfg_prim_nid,
 					      cfg->prcfg_cfg_nid,
 					      cfg->prcfg_mr);
 		mutex_unlock(&the_lnet.ln_api_mutex);
@@ -2742,7 +2742,7 @@ LNetCtl(unsigned int cmd, void *arg)
 			return -EINVAL;
 
 		mutex_lock(&the_lnet.ln_api_mutex);
-		rc = lnet_del_peer_ni_from_peer(cfg->prcfg_key_nid,
+		rc = lnet_del_peer_ni_from_peer(cfg->prcfg_prim_nid,
 						cfg->prcfg_cfg_nid);
 		mutex_unlock(&the_lnet.ln_api_mutex);
 		return rc;
@@ -2785,7 +2785,7 @@ LNetCtl(unsigned int cmd, void *arg)
 			     (cfg->prcfg_bulk + sizeof(*lpni_cri));
 
 		mutex_lock(&the_lnet.ln_api_mutex);
-		rc = lnet_get_peer_info(cfg->prcfg_idx, &cfg->prcfg_key_nid,
+		rc = lnet_get_peer_info(cfg->prcfg_idx, &cfg->prcfg_prim_nid,
 					&cfg->prcfg_cfg_nid, &cfg->prcfg_mr,
 					lpni_cri, lpni_stats);
 		mutex_unlock(&the_lnet.ln_api_mutex);
