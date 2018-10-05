0a378fad3923fccb0f45256c601501399dd9be44
Amir Shehata <amir.shehata@intel.com>
2018-10-05 10:30:04 +1000

    lustre: lnet: handle non-MR peers
    Add the ability to declare a peer to be non-MR from the DLC
    interface. By default if a peer is configured from DLC it is
    assumed to be MR capable, except when the non-mr flag is set.
    
    For non-MR peers always use the same NI to communicate with it.
    If multiple NIs are used to communicate with a non-MR peer the
    peer will consider that it's talking to different peers which could
    cause upper layers to be confused.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-7734
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Reviewed-on: http://review.whamcloud.com/19305
    Reviewed-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Reviewed-by: Olaf Weber <olaf@sgi.com>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/staging/lustre/lnet/lnet/api-ni.c
+++ drivers/staging/lustre/lnet/lnet/api-ni.c
@@ -2689,7 +2689,8 @@ LNetCtl(unsigned int cmd, void *arg)
 			return -EINVAL;
 
 		return lnet_add_peer_ni_to_peer(cfg->prcfg_key_nid,
-						cfg->prcfg_cfg_nid);
+						cfg->prcfg_cfg_nid,
+						cfg->prcfg_mr);
 	}
 
 	case IOC_LIBCFS_DEL_PEER_NI: {
