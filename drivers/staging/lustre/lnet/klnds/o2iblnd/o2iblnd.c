jasimmons1973/lustre

commit 10e138e41a4343fd1a88e4543990205d134e562a

lustre: o2iblnd: get IP address more directly.
Use dev_get_by_name() and for_primary_ifa() to
get IP address for a named device.  This is more
direct.

Reviewed-by: Doug Oucharek <dougso@me.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Signed-off-by: NeilBrown <neilb@suse.com>

drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd.c
@@ -38,6 +38,7 @@
 #include <asm/div64.h>
 #include <asm/page.h>
 #include "o2iblnd.h"
+#include <linux/inetdevice.h>
 
 static struct lnet_lnd the_o2iblnd;
 
@@ -2450,40 +2451,48 @@ void kiblnd_destroy_dev(struct kib_dev *dev)
 static struct kib_dev *kiblnd_create_dev(char *ifname)
 {
 	struct net_device *netdev;
+	struct in_device *in_dev;
 	struct kib_dev *dev;
-	__u32 netmask;
-	__u32 ip;
-	int up;
+	int flags;
 	int rc;
 
-	rc = lnet_ipif_query(ifname, &up, &ip, &netmask);
-	if (rc) {
-		CERROR("Can't query IPoIB interface %s: %d\n",
-		       ifname, rc);
-		return NULL;
+	rtnl_lock();
+	netdev = dev_get_by_name(&init_net, ifname);
+	if (!netdev) {
+		CERROR("Can't find IPoIB interface %s\n",
+		       ifname);
+		goto unlock;
 	}
 
-	if (!up) {
+	flags = dev_get_flags(netdev);
+	if (!(flags & IFF_UP)) {
 		CERROR("Can't query IPoIB interface %s: it's down\n", ifname);
+		rtnl_unlock();
 		return NULL;
 	}
 
 	dev = kzalloc(sizeof(*dev), GFP_NOFS);
 	if (!dev)
-		return NULL;
+		goto unlock;
 
-	netdev = dev_get_by_name(&init_net, ifname);
-	if (!netdev) {
-		dev->ibd_can_failover = 0;
-	} else {
-		dev->ibd_can_failover = !!(netdev->flags & IFF_MASTER);
-		dev_put(netdev);
-	}
+	dev->ibd_can_failover = !!(flags & IFF_MASTER);
 
 	INIT_LIST_HEAD(&dev->ibd_nets);
 	INIT_LIST_HEAD(&dev->ibd_list); /* not yet in kib_devs */
 	INIT_LIST_HEAD(&dev->ibd_fail_list);
-	dev->ibd_ifip = ip;
+	for_primary_ifa(in_dev)
+		if (strcmp(ifa->ifa_label, ifname) == 0) {
+			dev->ibd_ifip = ifa->ifa_local;
+			break;
+		}
+	endfor_ifa(in_dev);
+	rtnl_unlock();
+
+	if (dev->ibd_ifip == 0) {
+		CERROR("Can't initialize device: no IP address\n");
+		kfree(dev);
+		return NULL;
+	}
 	strcpy(&dev->ibd_ifname[0], ifname);
 
 	/* initialize the device */
@@ -2496,6 +2505,9 @@ static struct kib_dev *kiblnd_create_dev(char *ifname)
 
 	list_add_tail(&dev->ibd_list, &kiblnd_data.kib_devs);
 	return dev;
+unlock:
+	rtnl_unlock();
+	return NULL;
 }
 
 static void kiblnd_base_shutdown(void)

commit 9eb957b98aa6322abde33240bf50dd483c5d1190

lustre: lnd: resolve IP query code in LND drivers
The recent IP querying code that landed to both ksocklnd and
ko2iblnd have some bugs that prevent proper setup. The first
bug in both drivers is that the IP address of ifa_local is in
big endian format so on little endian systems the IP address
is incorrect. Calling ntohl() on ifa_local gets the real IP
address. The second bug located in ko2iblnd is that in_dev is
always NULL. Add the call __in_dev_get_rtnl() to get in_dev
and use that information to query the IP address.

Fixes: 10e138e41a43 ("lustre: o2iblnd: get IP address more directly.")
Fixes: f703f71afd98 ("lustre: socklnd:  use for_each_netdev() instead of lnet_ipif_enumerate()")
Signed-off-by: James Simmons <jsimmons@infradead.org>
Signed-off-by: NeilBrown <neilb@suse.com>

drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd.c
@@ -2461,8 +2461,7 @@ static struct kib_dev *kiblnd_create_dev(char *ifname)
 	flags = dev_get_flags(netdev);
 	if (!(flags & IFF_UP)) {
 		CERROR("Can't query IPoIB interface %s: it's down\n", ifname);
-		rtnl_unlock();
-		return NULL;
+		goto unlock;
 	}
 
 	dev = kzalloc(sizeof(*dev), GFP_NOFS);
@@ -2474,9 +2473,16 @@ static struct kib_dev *kiblnd_create_dev(char *ifname)
 	INIT_LIST_HEAD(&dev->ibd_nets);
 	INIT_LIST_HEAD(&dev->ibd_list); /* not yet in kib_devs */
 	INIT_LIST_HEAD(&dev->ibd_fail_list);
+
+	in_dev = __in_dev_get_rtnl(netdev);
+	if (!in_dev) {
+		kfree(dev);
+		goto unlock;
+	}
+
 	for_primary_ifa(in_dev)
 		if (strcmp(ifa->ifa_label, ifname) == 0) {
-			dev->ibd_ifip = ifa->ifa_local;
+			dev->ibd_ifip = ntohl(ifa->ifa_local);
 			break;
 		}
 	endfor_ifa(in_dev);

drivers/staging/lustre/lnet/klnds/socklnd/socklnd.c
@@ -2588,7 +2588,7 @@ ksocknal_enumerate_interfaces(struct ksock_net *net, char *iname)
 		}
 		for_primary_ifa(in_dev)
 			if (strcmp(ifa->ifa_label, name) == 0) {
-				ksi->ksni_ipaddr = ifa->ifa_local;
+				ksi->ksni_ipaddr = ntohl(ifa->ifa_local);
 				ksi->ksni_netmask = ifa->ifa_mask;
 				strlcpy(ksi->ksni_name,
 					name, sizeof(ksi->ksni_name));