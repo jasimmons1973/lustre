da004d1ba1ebb93742678a814c2f06586d14cf29
Lai Siyao <lai.siyao@whamcloud.com>
2018-10-14 14:58:15 -0400

    lustre: llite: disable statahead if starting statahead fail
    Once starting statahead thread fails, it should disable statahead.
    Current code only does this when "sai != NULL", instead it should
    check whether current process is opening this dir, so for cases
    like current file is not the first dirent, or sai allocation fail,
    it won't retry statahead.
    
    Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
    WC-bug-id: https://jira.whamcloud.com/browse/LU-10165
    Reviewed-on: https://review.whamcloud.com/29817
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/staging/lustre/lustre/llite/statahead.c
+++ drivers/staging/lustre/lustre/llite/statahead.c
@@ -1515,6 +1515,9 @@ static int start_statahead_thread(struct inode *dir, struct dentry *dentry)
 	task = kthread_create(ll_statahead_thread, parent, "ll_sa_%u",
 			      lli->lli_opendir_pid);
 	if (IS_ERR(task)) {
+		spin_lock(&lli->lli_sa_lock);
+		lli->lli_sai = NULL;
+		spin_unlock(&lli->lli_sa_lock);
 		rc = PTR_ERR(task);
 		CERROR("can't start ll_sa thread, rc : %d\n", rc);
 		goto out;
@@ -1540,8 +1543,8 @@ out:
 	 * that subsequent stat won't waste time to try it.
 	 */
 	spin_lock(&lli->lli_sa_lock);
-	lli->lli_sa_enabled = 0;
-	lli->lli_sai = NULL;
+	if (lli->lli_opendir_pid == current->pid)
+		lli->lli_sa_enabled = 0;
 	spin_unlock(&lli->lli_sa_lock);
 	if (sai)
 		ll_sai_free(sai);
