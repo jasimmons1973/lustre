62076e4eeb699292e7b1d9b8ecc5ec14eb3128e3
Olaf Weber <olaf@sgi.com>
2018-11-05 14:38:55 +1100

    lustre: lnet: rename lnet_add/del_peer_ni_to/from_peer()
    Rename lnet_add_peer_ni_to_peer() to lnet_add_peer_ni(), and
    lnet_del_peer_ni_from_peer() to lnet_del_peer_ni().  This brings
    the function names closer to the ioctls they implement:
    IOCTL_LIBCFS_ADD_PEER_NI and IOCTL_LIBCFS_DEL_PEER_NI. These
    names are also a more accturate description their effect: adding
    or deleting an lnet_peer_ni to LNet.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-9480
    Signed-off-by: Olaf Weber <olaf@sgi.com>
    Reviewed-on: https://review.whamcloud.com/25778
    Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
    Reviewed-by: Amir Shehata <amir.shehata@intel.com>
    Tested-by: Amir Shehata <amir.shehata@intel.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/staging/lustre/lnet/lnet/api-ni.c
+++ drivers/staging/lustre/lnet/lnet/api-ni.c
@@ -2848,9 +2848,9 @@ LNetCtl(unsigned int cmd, void *arg)
 			return -EINVAL;
 
 		mutex_lock(&the_lnet.ln_api_mutex);
-		rc = lnet_add_peer_ni_to_peer(cfg->prcfg_prim_nid,
-					      cfg->prcfg_cfg_nid,
-					      cfg->prcfg_mr);
+		rc = lnet_add_peer_ni(cfg->prcfg_prim_nid,
+				      cfg->prcfg_cfg_nid,
+				      cfg->prcfg_mr);
 		mutex_unlock(&the_lnet.ln_api_mutex);
 		return rc;
 	}
@@ -2862,8 +2862,8 @@ LNetCtl(unsigned int cmd, void *arg)
 			return -EINVAL;
 
 		mutex_lock(&the_lnet.ln_api_mutex);
-		rc = lnet_del_peer_ni_from_peer(cfg->prcfg_prim_nid,
-						cfg->prcfg_cfg_nid);
+		rc = lnet_del_peer_ni(cfg->prcfg_prim_nid,
+				      cfg->prcfg_cfg_nid);
 		mutex_unlock(&the_lnet.ln_api_mutex);
 		return rc;
 	}
