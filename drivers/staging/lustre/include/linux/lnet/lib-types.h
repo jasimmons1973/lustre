85e86b99b2c9716ff6b67d1e1efc8d91f751117c
Olaf Weber <olaf@sgi.com>
2018-11-05 14:38:57 +1100

    lustre: lnet: preferred NIs for non-Multi-Rail peers
    When a node sends a message to a peer NI, there may be
    a preferred local NI that should be the source of the
    message. This is in particular the case for non-Multi-
    Rail (NMR) peers, as an NMR peer depends in some cases
    on the source address of a message to correctly identify
    its origin. (This as opposed to using a UUID provided by
    a higher protocol layer.)
    
    Implement this by keeping an array of preferred local
    NIDs in the lnet_peer_ni structure. The case where only
    a single NID needs to be stored is optimized so that this
    can be done without needing to allocate any memory.
    
    A flag in the lnet_peer_ni, LNET_PEER_NI_NON_MR_PREF,
    indicates that the preferred NI was automatically added
    for an NMR peer. Note that a peer which has not been
    explicitly configured as Multi-Rail will be treated as
    non-Multi-Rail until proven otherwise. These automatic
    preferences will be cleared if the peer is changed to
    Multi-Rail.
    
    - lnet_peer_ni_set_non_mr_pref_nid()
      set NMR preferred NI for peer_ni
    - lnet_peer_ni_clr_non_mr_pref_nid()
      clear NMR preferred NI for peer_ni
    - lnet_peer_clr_non_mr_pref_nids()
      clear NMR preferred NIs for all peer_ni
    
    - lnet_peer_add_pref_nid()
      add a preferred NID
    - lnet_peer_del_pref_nid()
      delete a preferred NID
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-9480
    Signed-off-by: Olaf Weber <olaf@sgi.com>
    Reviewed-on: https://review.whamcloud.com/25782
    Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
    Reviewed-by: Amir Shehata <amir.shehata@intel.com>
    Tested-by: Amir Shehata <amir.shehata@intel.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/staging/lustre/include/linux/lnet/lib-types.h
+++ drivers/staging/lustre/include/linux/lnet/lib-types.h
@@ -481,13 +481,19 @@ struct lnet_peer_ni {
 	unsigned int		 lpni_ping_feats;
 	/* routers on this peer */
 	struct list_head	 lpni_routes;
-	/* array of preferred local nids */
-	lnet_nid_t		*lpni_pref_nids;
+	/* preferred local nids: if only one, use lpni_pref.nid */
+	union lpni_pref {
+		lnet_nid_t	nid;
+		lnet_nid_t	*nids;
+	} lpni_pref;
 	/* number of preferred NIDs in lnpi_pref_nids */
 	u32			lpni_pref_nnids;
 	/* router checker state */
 	struct lnet_rc_data	*lpni_rcd;
 };
+
+/* Preferred path added due to traffic on non-MR peer_ni */
+#define LNET_PEER_NI_NON_MR_PREF	BIT(0)
 
 struct lnet_peer {
 	/* chain on global peer list */
