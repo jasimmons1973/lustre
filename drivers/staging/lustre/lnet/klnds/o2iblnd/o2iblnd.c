0de2f82f29166c225fa0dfca02b8ac5ba1d50088
Dmitry Eremin <dmitry.eremin@intel.com>
2018-11-05 14:39:28 +1100

    lustre: o2iblnd: limit cap.max_send_wr for MLX5
    Decrease cap.max_send_wr until it is accepted by rdma_create_qp()
    
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    WC-bug-id: https://jira.whamcloud.com/browse/LU-7124
    Reviewed-on: http://review.whamcloud.com/18347
    Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
    Reviewed-by: Doug Oucharek <dougso@me.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd.c
+++ drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd.c
@@ -812,7 +812,14 @@ struct kib_conn *kiblnd_create_conn(struct kib_peer_ni *peer_ni,
 
 	conn->ibc_sched = sched;
 
-	rc = rdma_create_qp(cmid, conn->ibc_hdev->ibh_pd, init_qp_attr);
+	do {
+		rc = rdma_create_qp(cmid, conn->ibc_hdev->ibh_pd, init_qp_attr);
+		if (!rc || init_qp_attr->cap.max_send_wr < 16)
+			break;
+
+		init_qp_attr->cap.max_send_wr -= init_qp_attr->cap.max_send_wr / 4;
+	} while (rc);
+
 	if (rc) {
 		CERROR("Can't create QP: %d, send_wr: %d, recv_wr: %d, send_sge: %d, recv_sge: %d\n",
 		       rc, init_qp_attr->cap.max_send_wr,
@@ -821,6 +828,10 @@ struct kib_conn *kiblnd_create_conn(struct kib_peer_ni *peer_ni,
 		       init_qp_attr->cap.max_recv_sge);
 		goto failed_2;
 	}
+
+	if (init_qp_attr->cap.max_send_wr != IBLND_SEND_WRS(conn))
+		CDEBUG(D_NET, "original send wr %d, created with %d\n",
+		       IBLND_SEND_WRS(conn), init_qp_attr->cap.max_send_wr);
 
 	kfree(init_qp_attr);
 
