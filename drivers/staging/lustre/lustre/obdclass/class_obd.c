e5329903982f59d262aa11d3dfc4a95844c4d3b5
NeilBrown <neilb@suse.com>
2019-02-11 16:09:06 +1100

    lustre: don't manage module refs in obd_class_open/close.
    Core Linux code for managed char-devs ensures that the relevant
    module is held active which a char-dev is open - see cdev_get()
    and cdev_put().
    So there is no need for lustre/obd_class to manage the module
    ref count as well.
    
    As this is all that obd_class_open and obd_class_close do, those
    functions can be removed.
    
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/staging/lustre/lustre/obdclass/class_obd.c
+++ drivers/staging/lustre/lustre/obdclass/class_obd.c
@@ -546,20 +546,6 @@ int class_handle_ioctl(unsigned int cmd, unsigned long arg)
 	return err;
 } /* class_handle_ioctl */
 
-/*  opening /dev/obd */
-static int obd_class_open(struct inode *inode, struct file *file)
-{
-	try_module_get(THIS_MODULE);
-	return 0;
-}
-
-/*  closing /dev/obd */
-static int obd_class_release(struct inode *inode, struct file *file)
-{
-	module_put(THIS_MODULE);
-	return 0;
-}
-
 /* to control /dev/obd */
 static long obd_class_ioctl(struct file *filp, unsigned int cmd,
 			    unsigned long arg)
@@ -577,9 +563,7 @@ static long obd_class_ioctl(struct file *filp, unsigned int cmd,
 /* declare character device */
 static const struct file_operations obd_psdev_fops = {
 	.owner		= THIS_MODULE,
-	.unlocked_ioctl	= obd_class_ioctl,	/* unlocked_ioctl */
-	.open		= obd_class_open,	/* open */
-	.release	= obd_class_release,	/* release */
+	.unlocked_ioctl	= obd_class_ioctl,
 };
 
 /* modules setup */
