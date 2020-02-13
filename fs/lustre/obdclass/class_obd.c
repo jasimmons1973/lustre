3ab078645aa3f88864133a47207453e236916ac0
Andreas Dilger <adilger@whamcloud.com>
2020-02-13 08:13:24 -0500

    lustre: misc: remove LIBCFS_IOC_DEBUG_MASK ioctl
    Remove the LIBCFS_IOC_DEBUG_MASK ioctl, since the debug and subsystem
    mask can be modified via sysfs for a long time, and tools have not
    used this ioctl since 2.6.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-6202
    Lustre-commit: 70f932c7bfc5 ("LU-6202 misc: remove LIBCFS_IOC_DEBUG_MASK ioctl")
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/33692
    Reviewed-by: Patrick Farrell <paf@cray.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/obdclass/class_obd.c
+++ fs/lustre/obdclass/class_obd.c
@@ -274,17 +274,8 @@ EXPORT_SYMBOL(obd_ioctl_getdata);
 int class_handle_ioctl(unsigned int cmd, unsigned long arg)
 {
 	struct obd_ioctl_data *data;
-	struct libcfs_debug_ioctl_data *debug_data;
 	struct obd_device *obd = NULL;
 	int err = 0, len = 0;
-
-	/* only for debugging */
-	if (cmd == LIBCFS_IOC_DEBUG_MASK) {
-		debug_data = (struct libcfs_debug_ioctl_data *)arg;
-		libcfs_subsystem_debug = debug_data->subs;
-		libcfs_debug = debug_data->debug;
-		return 0;
-	}
 
 	CDEBUG(D_IOCTL, "cmd = %x\n", cmd);
 	if (obd_ioctl_getdata(&data, &len, (void __user *)arg)) {
