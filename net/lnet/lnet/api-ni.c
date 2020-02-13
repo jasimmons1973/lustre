ed7389fa9f4ce54307ec97ced58fd1072833eb12
Sebastien Buisson <sbuisson@ddn.com>
2020-02-13 07:44:35 -0500

    lnet: check for asymmetrical route messages
    Asymmetrical routes can be an issue when debugging network,
    and allowing them also opens the door to attacks where hostile
    clients inject data to the servers.
    
    In order to prevent asymmetrical routes, add a new lnet kernel
    module option named 'lnet_drop_asym_route'. When set to non-zero,
    lnet_parse() will check if the message received from a remote peer
    is coming through a router that would normally be used by this node
    to reach the remote peer. If it is not the case, then it means we
    are dealing with an asymmetrical route message, and the message will
    be dropped.
    
    The check for asymmetrical route can also be switched on/off with
    the command 'lnetctl set drop_asym_route 0|1'. And this parameter is
    exported/imported in Yaml.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-11894
    Lustre-commit: 4932febc1213 ("LU-11894 lnet: check for asymmetrical route messages")
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Reviewed-on: https://review.whamcloud.com/34119
    Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
    Reviewed-by: Chris Horn <hornc@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- net/lnet/lnet/api-ni.c
+++ net/lnet/lnet/api-ni.c
@@ -126,6 +126,20 @@ module_param_call(lnet_peer_discovery_disabled, discovery_set, param_get_int,
 MODULE_PARM_DESC(lnet_peer_discovery_disabled,
 		 "Set to 1 to disable peer discovery on this node.");
 
+unsigned int lnet_drop_asym_route;
+static int drop_asym_route_set(const char *val, const struct kernel_param *kp);
+
+static struct kernel_param_ops param_ops_drop_asym_route = {
+	.set = drop_asym_route_set,
+	.get = param_get_int,
+};
+
+#define param_check_drop_asym_route(name, p)	\
+	__param_check(name, p, int)
+module_param(lnet_drop_asym_route, drop_asym_route, 0644);
+MODULE_PARM_DESC(lnet_drop_asym_route,
+		 "Set to 1 to drop asymmetrical route messages.");
+
 unsigned int lnet_transaction_timeout = 50;
 static int transaction_to_set(const char *val, const struct kernel_param *kp);
 static struct kernel_param_ops param_ops_transaction_timeout = {
@@ -285,6 +299,36 @@ discovery_set(const char *val, const struct kernel_param *kp)
 	lnet_net_unlock(LNET_LOCK_EX);
 
 	lnet_push_update_to_peers(1);
+
+	mutex_unlock(&the_lnet.ln_api_mutex);
+
+	return 0;
+}
+
+static int
+drop_asym_route_set(const char *val, const struct kernel_param *kp)
+{
+	int rc;
+	unsigned int *drop_asym_route = (unsigned int *)kp->arg;
+	unsigned long value;
+
+	rc = kstrtoul(val, 0, &value);
+	if (rc) {
+		CERROR("Invalid module parameter value for 'lnet_drop_asym_route'\n");
+		return rc;
+	}
+
+	/* The purpose of locking the api_mutex here is to ensure that
+	 * the correct value ends up stored properly.
+	 */
+	mutex_lock(&the_lnet.ln_api_mutex);
+
+	if (value == *drop_asym_route) {
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return 0;
+	}
+
+	*drop_asym_route = value;
 
 	mutex_unlock(&the_lnet.ln_api_mutex);
 
