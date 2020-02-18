61aeed37889652fde26225d6814451d9f985b7b3
Emoly Liu <emoly@whamcloud.com>
2020-02-18 11:25:36 -0500

    lustre: ptlrpc: check buffer length in lustre_msg_string()
    Check buffer length in lustre_msg_string() in case of any invalid
    access.
    
    Reported-by: Alibaba Cloud <yunye.ry@alibaba-inc.com>
    WC-bug-id: https://jira.whamcloud.com/browse/LU-12613
    Lustre-commit: 728c58d60fae ("LU-12613 ptlrpc: check buffer length in lustre_msg_string()")
    Signed-off-by: Emoly Liu <emoly@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/35932
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
    Reviewed-by: Yunye Ry <yunye.ry@alibaba-inc.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/ptlrpc/pack_generic.c
+++ fs/lustre/ptlrpc/pack_generic.c
@@ -712,6 +712,11 @@ char *lustre_msg_string(struct lustre_msg *m, u32 index, u32 max_len)
 		       m, index, blen);
 		return NULL;
 	}
+	if (blen > PTLRPC_MAX_BUFLEN) {
+		CERROR("buffer length of msg %p buffer[%d] is invalid(%d)\n",
+		       m, index, blen);
+		return NULL;
+	}
 
 	if (max_len == 0) {
 		if (slen != blen - 1) {
