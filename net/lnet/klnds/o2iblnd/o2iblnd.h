jasimmons1973/lustre

commit 0ac56201bdaf710fe619b05836b82b19884aa957

lnet: lnd: bring back concurrent_sends
Revert "lustre: lnd: remove concurrent_sends tunable"

This reverts commit 0d4b38f73774f8363d6c419b16d3b34d23ad1ca9.

WC-bug-id: https://jira.whamcloud.com/browse/LU-11931
Lustre-commit: 83e45ead69ba ("LU-11931 lnd: bring back concurrent_sends")
Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34646
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Chris Horn <hornc@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

net/lnet/klnds/o2iblnd/o2iblnd.h
@@ -136,7 +136,9 @@ extern struct kib_tunables  kiblnd_tunables;
 /* WRs and CQEs (per connection) */
 #define IBLND_RECV_WRS(c)	IBLND_RX_MSGS(c)
 
-#define IBLND_CQ_ENTRIES(c)	(IBLND_RECV_WRS(c) + kiblnd_send_wrs(c))
+#define IBLND_CQ_ENTRIES(c)	\
+	(IBLND_RECV_WRS(c) + 2 * kiblnd_concurrent_sends(c->ibc_version, \
+							 c->ibc_peer->ibp_ni))
 
 struct kib_hca_dev;
 
@@ -634,6 +636,26 @@ extern struct kib_data kiblnd_data;
 void kiblnd_hdev_destroy(struct kib_hca_dev *hdev);
 
 int kiblnd_msg_queue_size(int version, struct lnet_ni *ni);
+
+static inline int
+kiblnd_concurrent_sends(int version, struct lnet_ni *ni)
+{
+	struct lnet_ioctl_config_o2iblnd_tunables *tunables;
+	int concurrent_sends;
+
+	tunables = &ni->ni_lnd_tunables.lnd_tun_u.lnd_o2ib;
+	concurrent_sends = tunables->lnd_concurrent_sends;
+
+	if (version == IBLND_MSG_VERSION_1) {
+		if (concurrent_sends > IBLND_MSG_QUEUE_SIZE_V1 * 2)
+			return IBLND_MSG_QUEUE_SIZE_V1 * 2;
+
+		if (concurrent_sends < IBLND_MSG_QUEUE_SIZE_V1 / 2)
+			return IBLND_MSG_QUEUE_SIZE_V1 / 2;
+	}
+
+	return concurrent_sends;
+}
 
 static inline void
 kiblnd_hdev_addref_locked(struct kib_hca_dev *hdev)

net/lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -806,6 +806,7 @@ kiblnd_post_tx_locked(struct kib_conn *conn, struct kib_tx *tx, int credit)
 {
 	struct kib_msg *msg = tx->tx_msg;
 	struct kib_peer_ni *peer_ni = conn->ibc_peer;
+	struct lnet_ni *ni = peer_ni->ibp_ni;
 	int ver = conn->ibc_version;
 	int rc;
 	int done;
@@ -821,7 +822,7 @@ kiblnd_post_tx_locked(struct kib_conn *conn, struct kib_tx *tx, int credit)
 	LASSERT(conn->ibc_credits >= 0);
 	LASSERT(conn->ibc_credits <= conn->ibc_queue_depth);
 
-	if (conn->ibc_nsends_posted == conn->ibc_queue_depth) {
+	if (conn->ibc_nsends_posted == kiblnd_concurrent_sends(ver, ni)) {
 		/* tx completions outstanding... */
 		CDEBUG(D_NET, "%s: posted enough\n",
 		       libcfs_nid2str(peer_ni->ibp_nid));
@@ -976,7 +977,7 @@ kiblnd_check_sends_locked(struct kib_conn *conn)
 		return;
 	}
 
-	LASSERT(conn->ibc_nsends_posted <= conn->ibc_queue_depth);
+	LASSERT(conn->ibc_nsends_posted <= kiblnd_concurrent_sends(ver, ni));
 	LASSERT(!IBLND_OOB_CAPABLE(ver) ||
 		conn->ibc_noops_posted <= IBLND_OOB_MSGS(ver));
 	LASSERT(conn->ibc_reserved_credits >= 0);

net/lnet/klnds/o2iblnd/o2iblnd_modparams.c
@@ -109,7 +109,7 @@ MODULE_PARM_DESC(ib_mtu, "IB MTU 256/512/1024/2048/4096");
 
 static int concurrent_sends;
 module_param(concurrent_sends, int, 0444);
-MODULE_PARM_DESC(concurrent_sends, "send work-queue sizing (obsolete)");
+MODULE_PARM_DESC(concurrent_sends, "send work-queue sizing");
 
 static bool use_fastreg_gaps;
 module_param(use_fastreg_gaps, bool, 0444);
@@ -272,10 +272,33 @@ int kiblnd_tunables_setup(struct lnet_ni *ni)
 		tunables->lnd_peercredits_hiw = peer_credits_hiw;
 
 	if (tunables->lnd_peercredits_hiw < net_tunables->lct_peer_tx_credits / 2)
-		tunables->lnd_peercredits_hiw = net_tunables->lct_peer_tx_credits / 2;
+		tunables->lnd_peercredits_hiw =
+			net_tunables->lct_peer_tx_credits / 2;
 
 	if (tunables->lnd_peercredits_hiw >= net_tunables->lct_peer_tx_credits)
-		tunables->lnd_peercredits_hiw = net_tunables->lct_peer_tx_credits - 1;
+		tunables->lnd_peercredits_hiw =
+			net_tunables->lct_peer_tx_credits - 1;
+
+	if (tunables->lnd_concurrent_sends == 0)
+		tunables->lnd_concurrent_sends =
+			net_tunables->lct_peer_tx_credits;
+
+	if (tunables->lnd_concurrent_sends >
+	    net_tunables->lct_peer_tx_credits * 2)
+		tunables->lnd_concurrent_sends =
+			net_tunables->lct_peer_tx_credits * 2;
+
+	if (tunables->lnd_concurrent_sends <
+	    net_tunables->lct_peer_tx_credits / 2)
+		tunables->lnd_concurrent_sends =
+			net_tunables->lct_peer_tx_credits / 2;
+
+	if (tunables->lnd_concurrent_sends <
+	    net_tunables->lct_peer_tx_credits) {
+		CWARN("Concurrent sends %d is lower than message queue size: %d, performance may drop slightly.\n",
+		      tunables->lnd_concurrent_sends,
+		      net_tunables->lct_peer_tx_credits);
+	}
 
 	if (!tunables->lnd_fmr_pool_size)
 		tunables->lnd_fmr_pool_size = fmr_pool_size;
@@ -298,6 +321,7 @@ void kiblnd_tunables_init(void)
 	default_tunables.lnd_version = 0;
 	default_tunables.lnd_peercredits_hiw = peer_credits_hiw;
 	default_tunables.lnd_map_on_demand = map_on_demand;
+	default_tunables.lnd_concurrent_sends = concurrent_sends;
 	default_tunables.lnd_fmr_pool_size = fmr_pool_size;
 	default_tunables.lnd_fmr_flush_trigger = fmr_flush_trigger;
 	default_tunables.lnd_fmr_cache = fmr_cache;

commit bf670cd93e33ffd15c5a494002567f46a8f27c6d

lnet: use number of wrs to calculate CQEs
Using concurrent sends to calculate the number of CQEs results
in a small number of CQEs which exposes an issue where under
failure scenarios, example when a node reboots, there wouldn't
be enough CQEs available leading to IB_EVENT_QP_FATAL

Fixes: 0ac56201bdaf ("lnet: lnd: bring back concurrent_sends")
WC-bug-id: https://jira.whamcloud.com/browse/LU-12279
Lustre-commit: 24294b843f79 ("LU-12279 lnet: use number of wrs to calculate CQEs")
Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34945
Reviewed-by: Sonia Sharma <sharmaso@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

net/lnet/klnds/o2iblnd/o2iblnd.h
@@ -136,9 +136,7 @@ extern struct kib_tunables  kiblnd_tunables;
 /* WRs and CQEs (per connection) */
 #define IBLND_RECV_WRS(c)	IBLND_RX_MSGS(c)
 
-#define IBLND_CQ_ENTRIES(c)	\
-	(IBLND_RECV_WRS(c) + 2 * kiblnd_concurrent_sends(c->ibc_version, \
-							 c->ibc_peer->ibp_ni))
+#define IBLND_CQ_ENTRIES(c) (IBLND_RECV_WRS(c) + kiblnd_send_wrs(c))
 
 struct kib_hca_dev;
 