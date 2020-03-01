jasimmons1973/lustre

commit 866b783c757fea086370b6a9c0ca44f7b6697a22

lnet: Do not allow gateways on remote nets
A gateway needs to be reachable over some local interface.

WC-bug-id: https://jira.whamcloud.com/browse/LU-12411
Lustre-commit: 43b35351e9ca ("LU-12411 lnet: Do not allow gateways on remote nets")
Signed-off-by: Chris Horn <hornc@cray.com>
Reviewed-on: https://review.whamcloud.com/35198
Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-by: Sonia Sharma <sharmaso@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

net/lnet/lnet/router.c
@@ -436,6 +436,13 @@ lnet_add_route(u32 net, u32 hops, lnet_nid_t gateway,
 	if (lnet_islocalnet(net))
 		return -EEXIST;
 
+	if (!lnet_islocalnet(LNET_NIDNET(gateway))) {
+		CERROR("Cannot add route with gateway %s. There is no local interface configured on LNet %s\n",
+		       libcfs_nid2str(gateway),
+		       libcfs_net2str(LNET_NIDNET(gateway)));
+		return -EINVAL;
+	}
+
 	/* Assume net, route, all new */
 	route = kzalloc(sizeof(*route), GFP_NOFS);
 	rnet = kzalloc(sizeof(*rnet), GFP_NOFS);

commit 894ed8caf2f819462f129c10dfae9827abfa0f28

lnet: Return EHOSTUNREACH for unreachable gateway
Commit f1d0660a5bbe ("lnet: Do not allow gateways on remote nets")
contains a flaw in that it shouldn't be a fatal error to
encounter an unreachable gateway when parsing routes.  Parsing should
continue in case there are any valid, reachable routes that are being
added.  Returning EINAL here will cause a failure to load the LNet
module.  lnet_parse_route() explicitly allows for lnet_add_route() to
return EHOSTUNREACH for just this purpose.

Fixes: 866b783c757f ("lnet: Do not allow gateways on remote nets")
WC-bug-id: https://jira.whamcloud.com/browse/LU-12595
Lustre-commit: 7c12c24c8a10 ("LU-12595 lnet: Return EHOSTUNREACH for unreachable gateway")
Signed-off-by: Chris Horn <hornc@cray.com>
Reviewed-on: https://review.whamcloud.com/35630
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

net/lnet/lnet/router.c
@@ -448,7 +448,7 @@ lnet_add_route(u32 net, u32 hops, lnet_nid_t gateway,
 		CERROR("Cannot add route with gateway %s. There is no local interface configured on LNet %s\n",
 		       libcfs_nid2str(gateway),
 		       libcfs_net2str(LNET_NIDNET(gateway)));
-		return -EINVAL;
+		return -EHOSTUNREACH;
 	}
 
 	/* Assume net, route, all new */