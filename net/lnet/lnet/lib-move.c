jasimmons1973/lustre

commit 448796bfe16bd6097a4e3caaac4c9d6be97d6b33

lnet: Sync the start of discovery and monitor threads
The discovery thread starts up before the monitor thread so it may
issue PUTs or GETs before the monitor thread has a chance to
initialize its data structures (namely the_lnet.ln_mt_rstq). This can
result in an OOPs when we attempt to attach response trackers to MDs.

Introduce a completion to synchronize the startup of these threads.

WC-bug-id: https://jira.whamcloud.com/browse/LU-12537
Lustre-commit: 9283e2ed6655 ("LU-12537 lnet: Sync the start of discovery and monitor threads")
Signed-off-by: Chris Horn <hornc@cray.com>
Reviewed-on: https://review.whamcloud.com/35478
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

net/lnet/lnet/lib-move.c
@@ -3529,6 +3529,7 @@ lnet_recv_put(struct lnet_ni *ni, struct lnet_msg *msg)
 
 	lnet_build_msg_event(msg, LNET_EVENT_PUT);
 
+	wait_for_completion(&the_lnet.ln_started);
 	/*
 	 * Must I ACK?  If so I'll grab the ack_wmd out of the header and put
 	 * it back into the ACK during lnet_finalize()

include/linux/lnet/lib-types.h
@@ -1161,6 +1161,11 @@ struct lnet {
 	/* recovery eq handler */
 	struct lnet_handle_eq		ln_mt_eqh;
 
+	/*
+	 * Completed when the discovery and monitor threads can enter their
+	 * work loops
+	 */
+	struct completion		ln_started;
 };
 
 #endif

net/lnet/lnet/api-ni.c
@@ -1062,6 +1062,7 @@ lnet_prepare(lnet_pid_t requested_pid)
 	INIT_LIST_HEAD(&the_lnet.ln_mt_peerNIRecovq);
 	init_waitqueue_head(&the_lnet.ln_dc_waitq);
 	LNetInvalidateEQHandle(&the_lnet.ln_mt_eqh);
+	init_completion(&the_lnet.ln_started);
 
 	rc = lnet_descriptor_setup();
 	if (rc != 0)
@@ -2582,6 +2583,8 @@ LNetNIInit(lnet_pid_t requested_pid)
 	lnet_router_debugfs_init();
 
 	mutex_unlock(&the_lnet.ln_api_mutex);
+
+	complete_all(&the_lnet.ln_started);
 
 	/* wait for all routers to start */
 	lnet_wait_router_start();

net/lnet/lnet/peer.c
@@ -3258,6 +3258,8 @@ static int lnet_peer_discovery(void *arg)
 	struct lnet_peer *lp;
 	int rc;
 
+	wait_for_completion(&the_lnet.ln_started);
+
 	CDEBUG(D_NET, "started\n");
 
 	for (;;) {
@@ -3429,7 +3431,14 @@ void lnet_peer_discovery_stop(void)
 
 	LASSERT(the_lnet.ln_dc_state == LNET_DC_STATE_RUNNING);
 	the_lnet.ln_dc_state = LNET_DC_STATE_STOPPING;
-	wake_up(&the_lnet.ln_dc_waitq);
+
+	/* In the LNetNIInit() path we may be stopping discovery before it
+	 * entered its work loop
+	 */
+	if (!completion_done(&the_lnet.ln_started))
+		complete(&the_lnet.ln_started);
+	else
+		wake_up(&the_lnet.ln_dc_waitq);
 
 	wait_event(the_lnet.ln_dc_waitq,
 		   the_lnet.ln_dc_state == LNET_DC_STATE_SHUTDOWN);

commit 455a775c2010b8dcfa4976dc8db5a4cd4efe1770

lnet: move wait_for_completion to correct place.
A recent patch added a wait_for_completion() call
to the wrong function.

Fixes: 448796bfe16b ("lnet: Sync the start of discovery and monitor threads")
Reviewed-by: James Simmons <jsimmons@infradead.org>
Signed-off-by: NeilBrown <neilb@suse.de>

net/lnet/lnet/lib-move.c
@@ -3227,6 +3227,8 @@ lnet_monitor_thread(void *arg)
 	int interval;
 	time64_t now;
 
+	wait_for_completion(&the_lnet.ln_started);
+
 	/* The monitor thread takes care of the following:
 	 *  1. Checks the aliveness of routers
 	 *  2. Checks if there are messages on the resend queue to resend
@@ -3577,9 +3579,7 @@ lnet_recv_put(struct lnet_ni *ni, struct lnet_msg *msg)
 
 	lnet_build_msg_event(msg, LNET_EVENT_PUT);
 
-	wait_for_completion(&the_lnet.ln_started);
-	/*
-	 * Must I ACK?  If so I'll grab the ack_wmd out of the header and put
+	/* Must I ACK?  If so I'll grab the ack_wmd out of the header and put
 	 * it back into the ACK during lnet_finalize()
 	 */
 	msg->msg_ack = !lnet_is_wire_handle_none(&hdr->msg.put.ack_wmd) &&