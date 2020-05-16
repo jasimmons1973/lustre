68cf9a1262452862c39089a96a6ec0a7f48dfa92
Wang Shilong <wshilong@ddn.com>
2020-05-16 10:43:13 -0400

    lustre: llite: fix to make jobstats work for async ra
    We use workqueue to triger async readahead, this will make
    the problem that read jobstats are not accounted by real
    IO, we could fix this by overwritting jobid which inited in
    vvp_io_init().
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-13490
    Lustre-commit: da8972322134a ("LU-13490 lustre: fix to make jobstats work for async ra")
    Signed-off-by: Wang Shilong <wshilong@ddn.com>
    Reviewed-on: https://review.whamcloud.com/38426
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Ben Evans <beevans@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/llite/rw.c
+++ fs/lustre/llite/rw.c
@@ -629,6 +629,12 @@ static void ll_readahead_handle_work(struct work_struct *wq)
 	if (rc)
 		goto out_put_env;
 
+	/* overwrite jobid inited in vvp_io_init() */
+	if (strncmp(ll_i2info(inode)->lli_jobid, work->lrw_jobid,
+		    sizeof(work->lrw_jobid)))
+		memcpy(ll_i2info(inode)->lli_jobid, work->lrw_jobid,
+		       sizeof(work->lrw_jobid));
+
 	vvp_env_io(env)->vui_fd = fd;
 	io->ci_state = CIS_LOCKED;
 	io->ci_async_readahead = true;
@@ -1582,6 +1588,8 @@ static int kickoff_async_readahead(struct file *file, unsigned long pages)
 		ras->ras_next_readahead_idx = end_idx + 1;
 		ras->ras_async_last_readpage_idx = start_idx;
 		spin_unlock(&ras->ras_lock);
+		memcpy(lrw->lrw_jobid, ll_i2info(inode)->lli_jobid,
+		       sizeof(lrw->lrw_jobid));
 		ll_readahead_work_add(inode, lrw);
 	} else {
 		return -ENOMEM;
