jasimmons1973/lustre

commit 382731148e2431ad74a3c505f31e609d1803ef5e

lnet: socklnd: use interface index to track local addr
socklnd currently uses local IP addresses to track the local end of a
route or connection.  This will not work so well for IPv6 where
addresses are generally more dynamic - an interface can have several
addresses and they can come and go.

Even with IPv4, an interface can have multiple addresses, though this
is partially hidden as there we normally use aliases which make it
looks like different interfaces: eth0:1 eth0:2 etc.  These are really
all the same interface, just with different addresses.

It is really the local interface, rather than the local address,
which is important.  Choosing the right interface ensures the traffic
goes over the desired network hardware.  Using an address, as the code
currently does, is just a convenient indirection.

With IPv6, using the interface directly will be easier, and it is
quite possible with IPv4.

So: this patch changes ksock_route to store an interface index for the
source end of the route, and adds the index nubmer to ksock_interface.

lnet_sock_listen() loses the local IP address which is never used, and
lnet_sock_connect() now is passed a source interface rather than a
source IP address.
lock_sock_create() also gets a local source interface.

WC-bug-id: https://jira.whamcloud.com/browse/LU-10391
Lustre-commit: d720fbaadad93 ("LU-10391 socklnd: use interface index to track local addr")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Reviewed-on: https://review.whamcloud.com/37702
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Aurelien Degremont <degremoa@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

net/lnet/lnet/lib-socket.c
@@ -40,6 +40,7 @@
 /* For sys_open & sys_close */
 #include <linux/syscalls.h>
 #include <net/sock.h>
+#include <linux/inetdevice.h>
 
 #include <linux/lnet/lib-lnet.h>
 
@@ -154,10 +155,43 @@ lnet_sock_read(struct socket *sock, void *buffer, int nob, int timeout)
 }
 EXPORT_SYMBOL(lnet_sock_read);
 
-static struct socket *
-lnet_sock_create(u32 local_ip, int local_port, struct net *ns)
+int choose_ipv4_src(__u32 *ret, int interface, __u32 dst_ipaddr, struct net *ns)
 {
-	struct sockaddr_in locaddr;
+	struct net_device *dev;
+	struct in_device *in_dev;
+	int err;
+	const struct in_ifaddr *ifa;
+
+	rcu_read_lock();
+	dev = dev_get_by_index_rcu(ns, interface);
+	err = -EINVAL;
+	if (!dev || !(dev->flags & IFF_UP))
+		goto out;
+	in_dev = __in_dev_get_rcu(dev);
+	if (!in_dev)
+		goto out;
+	err = -ENOENT;
+	in_dev_for_each_ifa_rcu(ifa, in_dev) {
+		if (*ret == 0 ||
+		    ((dst_ipaddr ^ ntohl(ifa->ifa_local)) &
+		     ntohl(ifa->ifa_mask)) == 0) {
+			/* This address at least as good as what we
+			 * already have
+			 */
+			*ret = ntohl(ifa->ifa_local);
+			err = 0;
+		}
+	}
+out:
+	rcu_read_unlock();
+	return err;
+}
+EXPORT_SYMBOL(choose_ipv4_src);
+
+static struct socket *
+lnet_sock_create(int interface, struct sockaddr *remaddr,
+		 int local_port, struct net *ns)
+{
 	struct socket *sock;
 	int rc;
 	int option;
@@ -176,14 +210,25 @@ lnet_sock_create(u32 local_ip, int local_port, struct net *ns)
 		goto failed;
 	}
 
-	if (local_ip || local_port) {
-		memset(&locaddr, 0, sizeof(locaddr));
+	if (interface >= 0 || local_port) {
+		struct sockaddr_in locaddr = {};
+
 		locaddr.sin_family = AF_INET;
+		locaddr.sin_addr.s_addr = INADDR_ANY;
+		if (interface >= 0) {
+			struct sockaddr_in *sin = (void *)remaddr;
+			u32 ip;
+
+			rc = choose_ipv4_src(&ip,
+					     interface,
+					     ntohl(sin->sin_addr.s_addr),
+					     ns);
+			if (rc)
+				goto failed;
+			locaddr.sin_addr.s_addr = htonl(ip);
+		}
+
 		locaddr.sin_port = htons(local_port);
-		if (!local_ip)
-			locaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-		else
-			locaddr.sin_addr.s_addr = htonl(local_ip);
 
 		rc = kernel_bind(sock, (struct sockaddr *)&locaddr,
 				 sizeof(locaddr));
@@ -275,12 +320,12 @@ lnet_sock_getbuf(struct socket *sock, int *txbufsize, int *rxbufsize)
 EXPORT_SYMBOL(lnet_sock_getbuf);
 
 struct socket *
-lnet_sock_listen(u32 local_ip, int local_port, int backlog, struct net *ns)
+lnet_sock_listen(int local_port, int backlog, struct net *ns)
 {
 	struct socket *sock;
 	int rc;
 
-	sock = lnet_sock_create(local_ip, local_port, ns);
+	sock = lnet_sock_create(-1, NULL, local_port, ns);
 	if (IS_ERR(sock)) {
 		rc = PTR_ERR(sock);
 		if (rc == -EADDRINUSE)
@@ -299,7 +344,7 @@ lnet_sock_listen(u32 local_ip, int local_port, int backlog, struct net *ns)
 }
 
 struct socket *
-lnet_sock_connect(u32 local_ip, int local_port,
+lnet_sock_connect(int interface, int local_port,
 		  u32 peer_ip, int peer_port,
 		  struct net *ns)
 {
@@ -307,14 +352,15 @@ lnet_sock_connect(u32 local_ip, int local_port,
 	struct socket *sock;
 	int rc;
 
-	sock = lnet_sock_create(local_ip, local_port, ns);
-	if (IS_ERR(sock))
-		return sock;
-
 	memset(&srvaddr, 0, sizeof(srvaddr));
 	srvaddr.sin_family = AF_INET;
 	srvaddr.sin_port = htons(peer_port);
 	srvaddr.sin_addr.s_addr = htonl(peer_ip);
+
+	sock = lnet_sock_create(interface, (struct sockaddr *)&srvaddr,
+				local_port, ns);
+	if (IS_ERR(sock))
+		return sock;
 
 	rc = kernel_connect(sock, (struct sockaddr *)&srvaddr,
 			    sizeof(srvaddr), 0);
@@ -328,8 +374,8 @@ lnet_sock_connect(u32 local_ip, int local_port,
 	 * port...
 	 */
 	CDEBUG_LIMIT(rc == -EADDRNOTAVAIL ? D_NET : D_NETERROR,
-		     "Error %d connecting %pI4h/%d -> %pI4h/%d\n", rc,
-		     &local_ip, local_port, &peer_ip, peer_port);
+		     "Error %d connecting %d -> %pI4h/%d\n", rc,
+		     local_port, &peer_ip, peer_port);
 
 	sock_release(sock);
 	return ERR_PTR(rc);

include/linux/lnet/lib-lnet.h
@@ -85,6 +85,8 @@ extern unsigned int lnet_current_net_count;
 #define DEFAULT_PEER_TIMEOUT    180
 #define LNET_LND_DEFAULT_TIMEOUT 5
 
+int choose_ipv4_src(u32 *ret, int interface, u32 dst_ipaddr, struct net *ns);
+
 bool lnet_is_route_alive(struct lnet_route *route);
 
 #define LNET_SMALL_MD_SIZE	offsetof(struct lnet_libmd, md_iov.iov[1])
@@ -682,7 +684,7 @@ unsigned int lnet_get_lnd_timeout(void);
 void lnet_register_lnd(struct lnet_lnd *lnd);
 void lnet_unregister_lnd(struct lnet_lnd *lnd);
 
-struct socket *lnet_connect(lnet_nid_t peer_nid, u32 local_ip, u32 peer_ip,
+struct socket *lnet_connect(lnet_nid_t peer_nid, int interface, u32 peer_ip,
 			    int peer_port, struct net *ns);
 void lnet_connect_console_error(int rc, lnet_nid_t peer_nid,
 				u32 peer_ip, int port);
@@ -711,11 +713,10 @@ int lnet_sock_getaddr(struct socket *socket, bool remote, u32 *ip, int *port);
 int lnet_sock_write(struct socket *sock, void *buffer, int nob, int timeout);
 int lnet_sock_read(struct socket *sock, void *buffer, int nob, int timeout);
 
-struct socket *lnet_sock_listen(u32 ip, int port, int backlog,
+struct socket *lnet_sock_listen(int port, int backlog,
 				struct net *ns);
-struct socket *lnet_sock_connect(u32 local_ip, int local_port,
+struct socket *lnet_sock_connect(int interface, int local_port,
 				 u32 peer_ip, int peer_port, struct net *ns);
-void libcfs_sock_release(struct socket *sock);
 
 int lnet_peers_start_down(void);
 int lnet_peer_buffer_credits(struct lnet_net *net);

net/lnet/klnds/socklnd/socklnd.c
@@ -39,8 +39,8 @@
  */
 
 #include <linux/pci.h>
-#include "socklnd.h"
 #include <linux/inetdevice.h>
+#include "socklnd.h"
 
 static struct lnet_lnd the_ksocklnd;
 struct ksock_nal_data ksocknal_data;
@@ -63,6 +63,57 @@ ksocknal_ip2iface(struct lnet_ni *ni, u32 ip)
 	return NULL;
 }
 
+static struct ksock_interface *
+ksocknal_index2iface(struct lnet_ni *ni, int index)
+{
+	struct ksock_net *net = ni->ni_data;
+	int i;
+	struct ksock_interface *iface;
+
+	for (i = 0; i < net->ksnn_ninterfaces; i++) {
+		LASSERT(i < LNET_INTERFACES_NUM);
+		iface = &net->ksnn_interfaces[i];
+
+		if (iface->ksni_index == index)
+			return iface;
+	}
+
+	return NULL;
+}
+
+static int ksocknal_ip2index(__u32 ipaddress, struct lnet_ni *ni)
+{
+	struct net_device *dev;
+	int ret = -1;
+	const struct in_ifaddr *ifa;
+
+	rcu_read_lock();
+	for_each_netdev(ni->ni_net_ns, dev) {
+		int flags = dev_get_flags(dev);
+		struct in_device *in_dev;
+
+		if (flags & IFF_LOOPBACK) /* skip the loopback IF */
+			continue;
+
+		if (!(flags & IFF_UP))
+			continue;
+
+		in_dev = __in_dev_get_rcu(dev);
+		if (!in_dev)
+			continue;
+
+		in_dev_for_each_ifa_rcu(ifa, in_dev) {
+			if (ntohl(ifa->ifa_local) == ipaddress)
+				ret = dev->ifindex;
+		}
+		if (ret >= 0)
+			break;
+	}
+	rcu_read_unlock();
+
+	return ret;
+}
+
 static struct ksock_route *
 ksocknal_create_route(u32 ipaddr, int port)
 {
@@ -76,6 +127,7 @@ ksocknal_create_route(u32 ipaddr, int port)
 	route->ksnr_peer = NULL;
 	route->ksnr_retry_interval = 0;		/* OK to connect at any time */
 	route->ksnr_ipaddr = ipaddr;
+	route->ksnr_myiface = -1;
 	route->ksnr_port = port;
 	route->ksnr_scheduled = 0;
 	route->ksnr_connecting = 0;
@@ -287,12 +339,13 @@ ksocknal_get_peer_info(struct lnet_ni *ni, int index,
 				continue;
 
 			*id = peer_ni->ksnp_id;
-			*myip = route->ksnr_myipaddr;
+			rc = choose_ipv4_src(myip, route->ksnr_myiface,
+					     route->ksnr_ipaddr,
+					     ni->ni_net_ns);
 			*peer_ip = route->ksnr_ipaddr;
 			*port = route->ksnr_port;
 			*conn_count = route->ksnr_conn_count;
 			*share_count = route->ksnr_share_count;
-			rc = 0;
 			goto out;
 		}
 	}
@@ -308,32 +361,35 @@ ksocknal_associate_route_conn_locked(struct ksock_route *route,
 	struct ksock_peer_ni *peer_ni = route->ksnr_peer;
 	int type = conn->ksnc_type;
 	struct ksock_interface *iface;
+	int conn_iface = ksocknal_ip2index(conn->ksnc_myipaddr,
+					   route->ksnr_peer->ksnp_ni);
 
 	conn->ksnc_route = route;
 	ksocknal_route_addref(route);
 
-	if (route->ksnr_myipaddr != conn->ksnc_myipaddr) {
-		if (!route->ksnr_myipaddr) {
+	if (route->ksnr_myiface != conn_iface) {
+		if (route->ksnr_myiface < 0) {
 			/* route wasn't bound locally yet (the initial route) */
-			CDEBUG(D_NET, "Binding %s %pI4h to %pI4h\n",
+			CDEBUG(D_NET, "Binding %s %pI4h to interface %d\n",
 			       libcfs_id2str(peer_ni->ksnp_id),
 			       &route->ksnr_ipaddr,
-			       &conn->ksnc_myipaddr);
+			       conn_iface);
 		} else {
-			CDEBUG(D_NET, "Rebinding %s %pI4h from %pI4h to %pI4h\n",
+			CDEBUG(D_NET,
+			       "Rebinding %s %pI4h from interface %d to %d\n",
 			       libcfs_id2str(peer_ni->ksnp_id),
 			       &route->ksnr_ipaddr,
-			       &route->ksnr_myipaddr,
-			       &conn->ksnc_myipaddr);
+			       route->ksnr_myiface,
+			       conn_iface);
 
-			iface = ksocknal_ip2iface(route->ksnr_peer->ksnp_ni,
-						  route->ksnr_myipaddr);
+			iface = ksocknal_index2iface(route->ksnr_peer->ksnp_ni,
+						     route->ksnr_myiface);
 			if (iface)
 				iface->ksni_nroutes--;
 		}
-		route->ksnr_myipaddr = conn->ksnc_myipaddr;
-		iface = ksocknal_ip2iface(route->ksnr_peer->ksnp_ni,
-					  route->ksnr_myipaddr);
+		route->ksnr_myiface = conn_iface;
+		iface = ksocknal_index2iface(route->ksnr_peer->ksnp_ni,
+					     route->ksnr_myiface);
 		if (iface)
 			iface->ksni_nroutes++;
 	}
@@ -405,9 +461,9 @@ ksocknal_del_route_locked(struct ksock_route *route)
 		ksocknal_close_conn_locked(conn, 0);
 	}
 
-	if (route->ksnr_myipaddr) {
-		iface = ksocknal_ip2iface(route->ksnr_peer->ksnp_ni,
-					  route->ksnr_myipaddr);
+	if (route->ksnr_myiface >= 0) {
+		iface = ksocknal_index2iface(route->ksnr_peer->ksnp_ni,
+					     route->ksnr_myiface);
 		if (iface)
 			iface->ksni_nroutes--;
 	}
@@ -897,7 +953,7 @@ ksocknal_create_routes(struct ksock_peer_ni *peer_ni, int port,
 			/* Using this interface already? */
 			list_for_each_entry(route, &peer_ni->ksnp_routes,
 					    ksnr_list)
-				if (route->ksnr_myipaddr == iface->ksni_ipaddr)
+				if (route->ksnr_myiface == iface->ksni_index)
 					goto next_iface;
 
 			this_netmatch = (!((iface->ksni_ipaddr ^
@@ -920,7 +976,7 @@ next_iface:
 		if (!best_iface)
 			continue;
 
-		newroute->ksnr_myipaddr = best_iface->ksni_ipaddr;
+		newroute->ksnr_myiface = best_iface->ksni_index;
 		best_iface->ksni_nroutes++;
 
 		ksocknal_add_route_locked(peer_ni, newroute);
@@ -1894,6 +1950,7 @@ ksocknal_add_interface(struct lnet_ni *ni, u32 ipaddress, u32 netmask)
 	} else {
 		iface = &net->ksnn_interfaces[net->ksnn_ninterfaces++];
 
+		iface->ksni_index = ksocknal_ip2index(ipaddress, ni);
 		iface->ksni_ipaddr = ipaddress;
 		iface->ksni_netmask = netmask;
 		iface->ksni_nroutes = 0;
@@ -1906,7 +1963,8 @@ ksocknal_add_interface(struct lnet_ni *ni, u32 ipaddress, u32 netmask)
 
 			list_for_each_entry(route, &peer_ni->ksnp_routes,
 					    ksnr_list) {
-				if (route->ksnr_myipaddr == ipaddress)
+				if (route->ksnr_myiface ==
+				    iface->ksni_index)
 					iface->ksni_nroutes++;
 			}
 		}
@@ -1924,7 +1982,8 @@ ksocknal_add_interface(struct lnet_ni *ni, u32 ipaddress, u32 netmask)
 }
 
 static void
-ksocknal_peer_del_interface_locked(struct ksock_peer_ni *peer_ni, u32 ipaddr)
+ksocknal_peer_del_interface_locked(struct ksock_peer_ni *peer_ni,
+				   u32 ipaddr, int index)
 {
 	struct list_head *tmp;
 	struct list_head *nxt;
@@ -1945,12 +2004,12 @@ ksocknal_peer_del_interface_locked(struct ksock_peer_ni *peer_ni, u32 ipaddr)
 	list_for_each_safe(tmp, nxt, &peer_ni->ksnp_routes) {
 		route = list_entry(tmp, struct ksock_route, ksnr_list);
 
-		if (route->ksnr_myipaddr != ipaddr)
+		if (route->ksnr_myiface != index)
 			continue;
 
 		if (route->ksnr_share_count) {
 			/* Manually created; keep, but unbind */
-			route->ksnr_myipaddr = 0;
+			route->ksnr_myiface = -1;
 		} else {
 			ksocknal_del_route_locked(route);
 		}
@@ -1972,8 +2031,11 @@ ksocknal_del_interface(struct lnet_ni *ni, u32 ipaddress)
 	struct hlist_node *nxt;
 	struct ksock_peer_ni *peer_ni;
 	u32 this_ip;
+	int index;
 	int i;
 	int j;
+
+	index = ksocknal_ip2index(ipaddress, ni);
 
 	write_lock_bh(&ksocknal_data.ksnd_global_lock);
 
@@ -1996,7 +2058,8 @@ ksocknal_del_interface(struct lnet_ni *ni, u32 ipaddress)
 			if (peer_ni->ksnp_ni != ni)
 				continue;
 
-			ksocknal_peer_del_interface_locked(peer_ni, this_ip);
+			ksocknal_peer_del_interface_locked(peer_ni,
+							   this_ip, index);
 		}
 	}
 

net/lnet/klnds/socklnd/socklnd.h
@@ -99,6 +99,7 @@ struct ksock_sched {				/* per scheduler state */
 #define KSOCK_THREAD_SID(id)		((id) & ((1UL << KSOCK_CPT_SHIFT) - 1))
 
 struct ksock_interface {			/* in-use interface */
+	int		ksni_index;		/* Linux interface index */
 	u32		ksni_ipaddr;		/* interface's IP address */
 	u32		ksni_netmask;		/* interface's network mask */
 	int		ksni_nroutes;		/* # routes using (active) */
@@ -391,7 +392,7 @@ struct ksock_route {
 							 * can happen next
 							 */
 	time64_t		ksnr_retry_interval;	/* how long between retries */
-	u32			ksnr_myipaddr;		/* my IP */
+	int			ksnr_myiface;		/* my IP */
 	u32			ksnr_ipaddr;		/* IP address to connect to */
 	int			ksnr_port;		/* port to connect to */
 	unsigned int		ksnr_scheduled:1;	/* scheduled for attention */

net/lnet/klnds/socklnd/socklnd_cb.c
@@ -1868,7 +1868,7 @@ ksocknal_connect(struct ksock_route *route)
 		}
 
 		sock = lnet_connect(peer_ni->ksnp_id.nid,
-				    route->ksnr_myipaddr,
+				    route->ksnr_myiface,
 				    route->ksnr_ipaddr, route->ksnr_port,
 				    peer_ni->ksnp_ni->ni_net_ns);
 		if (IS_ERR(sock)) {

net/lnet/lnet/acceptor.c
@@ -131,7 +131,7 @@ lnet_connect_console_error(int rc, lnet_nid_t peer_nid,
 EXPORT_SYMBOL(lnet_connect_console_error);
 
 struct socket *
-lnet_connect(lnet_nid_t peer_nid, u32 local_ip, u32 peer_ip,
+lnet_connect(lnet_nid_t peer_nid, int interface, u32 peer_ip,
 	     int peer_port, struct net *ns)
 {
 	struct lnet_acceptor_connreq cr;
@@ -146,7 +146,7 @@ lnet_connect(lnet_nid_t peer_nid, u32 local_ip, u32 peer_ip,
 	     --port) {
 		/* Iterate through reserved ports. */
 
-		sock = lnet_sock_connect(local_ip, port,
+		sock = lnet_sock_connect(interface, port,
 					 peer_ip, peer_port, ns);
 		if (IS_ERR(sock)) {
 			rc = PTR_ERR(sock);
@@ -330,7 +330,7 @@ lnet_acceptor(void *arg)
 	LASSERT(!lnet_acceptor_state.pta_sock);
 
 	lnet_acceptor_state.pta_sock =
-		lnet_sock_listen(0, accept_port, accept_backlog,
+		lnet_sock_listen(accept_port, accept_backlog,
 				 lnet_acceptor_state.pta_ns);
 	if (IS_ERR(lnet_acceptor_state.pta_sock)) {
 		rc = PTR_ERR(lnet_acceptor_state.pta_sock);

commit c368256c16e03c5cd445625d4d18862faedcffd6

lnet: fix uninitialize var in choose_ipv4_src()
choose_ip4_src() test "*ret" without initializing it - and callers do
not (and should not) initialize the var.

Instead of testing "*ret", test "err" - if this is non-zero (it will
be -ENOENT) we want to use the address.  If it is zero, then we only
use the address if it is on the right subnet.

Reported-by: Amir Shehata <ashehata@whamcloud.com>
Fixes: 382731148e24 ("lnet: socklnd: use interface index to track local addr")
WC-bug-id: https://jira.whamcloud.com/browse/LU-10391
Lustre-commit: 8ea895389a92d ("LU-10391 lnet: fix uninitialize var in choose_ipv4_src()")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Reviewed-on: https://review.whamcloud.com/38823
Reviewed-by: Aurelien Degremont <degremoa@amazon.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

net/lnet/lnet/lib-socket.c
@@ -158,7 +158,7 @@ int choose_ipv4_src(__u32 *ret, int interface, __u32 dst_ipaddr, struct net *ns)
 		goto out;
 	err = -ENOENT;
 	in_dev_for_each_ifa_rcu(ifa, in_dev) {
-		if (*ret == 0 ||
+		if (err == 0 ||
 		    ((dst_ipaddr ^ ntohl(ifa->ifa_local)) &
 		     ntohl(ifa->ifa_mask)) == 0) {
 			/* This address at least as good as what we