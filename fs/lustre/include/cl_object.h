jasimmons1973/lustre

commit ebdbecbaf50b1dfeb22f206fbca0be8267f60f96

lustre: obdclass: use slab allocation for cl_dio_aio
cl_dio_aio is used frequently for dio/aio, try to use
a private slab pool for it.

This could help improve aio performance.

WC-bug-id: https://jira.whamcloud.com/browse/LU-13134
Lustre-commit: 3db1d3322eb0 ("LU-13134 obdclass: use slab allocation for cl_dio_aio")
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/37227
Reviewed-by: Patrick Farrell <farr0186@gmail.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/include/cl_object.h
@@ -2481,6 +2481,7 @@ int cl_sync_io_wait(const struct lu_env *env, struct cl_sync_io *anchor,
 		    long timeout);
 void cl_sync_io_note(const struct lu_env *env, struct cl_sync_io *anchor,
 		     int ioret);
+struct cl_dio_aio *cl_aio_alloc(struct kiocb *iocb);
 static inline void cl_sync_io_init(struct cl_sync_io *anchor, int nr)
 {
 	cl_sync_io_init_notify(anchor, nr, NULL, NULL);

fs/lustre/llite/rw26.c
@@ -181,12 +181,6 @@ static ssize_t ll_get_user_pages(int rw, struct iov_iter *iter,
 	return result;
 }
 
-static inline void aio_complete(struct kiocb *iocb, ssize_t res, ssize_t res2)
-{
-	if (iocb->ki_complete)
-		iocb->ki_complete(iocb, res, res2);
-}
-
 /* direct IO pages */
 struct ll_dio_pages {
 	struct cl_dio_aio	*ldp_aio;
@@ -200,43 +194,6 @@ struct ll_dio_pages {
 	/* the file offset of the first page. */
 	loff_t			ldp_file_offset;
 };
-
-void ll_aio_end(const struct lu_env *env, struct cl_sync_io *anchor)
-{
-	struct cl_dio_aio *aio = container_of(anchor, typeof(*aio), cda_sync);
-	ssize_t ret = anchor->csi_sync_rc;
-
-	/* release pages */
-	while (aio->cda_pages.pl_nr > 0) {
-		struct cl_page *page = cl_page_list_first(&aio->cda_pages);
-
-		cl_page_get(page);
-		cl_page_list_del(env, &aio->cda_pages, page);
-		cl_page_delete(env, page);
-		cl_page_put(env, page);
-	}
-
-	if (!is_sync_kiocb(aio->cda_iocb))
-		aio_complete(aio->cda_iocb, ret ?: aio->cda_bytes, 0);
-}
-
-static struct cl_dio_aio *ll_aio_alloc(struct kiocb *iocb)
-{
-	struct cl_dio_aio *aio;
-
-	aio = kzalloc(sizeof(*aio), GFP_NOFS);
-	if (aio) {
-		/*
-		 * Hold one ref so that it won't be released until
-		 * every pages is added.
-		 */
-		cl_sync_io_init_notify(&aio->cda_sync, 1, is_sync_kiocb(iocb) ?
-				       NULL : aio, ll_aio_end);
-		cl_page_list_init(&aio->cda_pages);
-		aio->cda_iocb = iocb;
-	}
-	return aio;
-}
 
 static int
 ll_direct_rw_pages(const struct lu_env *env, struct cl_io *io, size_t size,
@@ -361,7 +318,7 @@ static ssize_t ll_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	io = lcc->lcc_io;
 	LASSERT(io);
 
-	aio = ll_aio_alloc(iocb);
+	aio = cl_aio_alloc(iocb);
 	if (!aio)
 		return -ENOMEM;
 

fs/lustre/obdclass/cl_io.c
@@ -1082,6 +1082,47 @@ int cl_sync_io_wait(const struct lu_env *env, struct cl_sync_io *anchor,
 }
 EXPORT_SYMBOL(cl_sync_io_wait);
 
+static void cl_aio_end(const struct lu_env *env, struct cl_sync_io *anchor)
+{
+	struct cl_dio_aio *aio = container_of(anchor, typeof(*aio), cda_sync);
+	ssize_t ret = anchor->csi_sync_rc;
+
+	/* release pages */
+	while (aio->cda_pages.pl_nr > 0) {
+		struct cl_page *page = cl_page_list_first(&aio->cda_pages);
+
+		cl_page_get(page);
+		cl_page_list_del(env, &aio->cda_pages, page);
+		cl_page_delete(env, page);
+		cl_page_put(env, page);
+	}
+
+	if (!is_sync_kiocb(aio->cda_iocb) &&
+	    aio->cda_iocb->ki_complete)
+		aio->cda_iocb->ki_complete(aio->cda_iocb,
+					   ret ?: aio->cda_bytes, 0);
+}
+
+struct cl_dio_aio *cl_aio_alloc(struct kiocb *iocb)
+{
+	struct cl_dio_aio *aio;
+
+	aio = kmem_cache_zalloc(cl_dio_aio_kmem, GFP_NOFS);
+	if (aio) {
+		/*
+		 * Hold one ref so that it won't be released until
+		 * every pages is added.
+		 */
+		cl_sync_io_init_notify(&aio->cda_sync, 1,
+				       is_sync_kiocb(iocb) ? NULL : aio,
+				       cl_aio_end);
+		cl_page_list_init(&aio->cda_pages);
+		aio->cda_iocb = iocb;
+	}
+	return aio;
+}
+EXPORT_SYMBOL(cl_aio_alloc);
+
 /**
  * Indicate that transfer of a single page completed.
  */
@@ -1119,7 +1160,8 @@ void cl_sync_io_note(const struct lu_env *env, struct cl_sync_io *anchor,
 		 * If anchor->csi_aio is set, we are responsible for freeing
 		 * memory here rather than when cl_sync_io_wait() completes.
 		 */
-		kfree(aio);
+		if (aio)
+			kmem_cache_free(cl_dio_aio_kmem, aio);
 	}
 }
 EXPORT_SYMBOL(cl_sync_io_note);

fs/lustre/obdclass/cl_internal.h
@@ -47,6 +47,8 @@ struct cl_thread_info {
 	struct cl_sync_io	clt_anchor;
 };
 
+extern struct kmem_cache *cl_dio_aio_kmem;
+
 struct cl_thread_info *cl_env_info(const struct lu_env *env);
 
 #endif /* _CL_INTERNAL_H */

fs/lustre/obdclass/cl_object.c
@@ -57,6 +57,7 @@
 #include "cl_internal.h"
 
 static struct kmem_cache *cl_env_kmem;
+struct kmem_cache *cl_dio_aio_kmem;
 
 /** Lock class of cl_object_header::coh_attr_guard */
 static struct lock_class_key cl_attr_guard_class;
@@ -985,6 +986,11 @@ static struct lu_kmem_descr cl_object_caches[] = {
 		.ckd_cache = &cl_env_kmem,
 		.ckd_name  = "cl_env_kmem",
 		.ckd_size  = sizeof(struct cl_env)
+	},
+	{
+		.ckd_cache = &cl_dio_aio_kmem,
+		.ckd_name  = "cl_dio_aio_kmem",
+		.ckd_size  = sizeof(struct cl_dio_aio)
 	},
 	{
 		.ckd_cache = NULL

commit cdb08ba510f6c6b1e64560c5e875472f59c7cca8

lustre: llite: fix to free cl_dio_aio properly
@cl_dio_aio is allocated by slab, we should use slab
free helper to free its memory.

Fixes: ebdbecbaf50b ("lustre: obdclass: use slab allocation for cl_dio_aio")
WC-bug-id: https://jira.whamcloud.com/browse/LU-13134
Lustre-commit: f71a539c3e41b ("LU-13134 llite: fix to free cl_dio_aio properly")
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/39103
Reviewed-by: Patrick Farrell <farr0186@gmail.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/include/cl_object.h
@@ -2538,6 +2538,8 @@ int cl_sync_io_wait(const struct lu_env *env, struct cl_sync_io *anchor,
 void cl_sync_io_note(const struct lu_env *env, struct cl_sync_io *anchor,
 		     int ioret);
 struct cl_dio_aio *cl_aio_alloc(struct kiocb *iocb);
+void cl_aio_free(struct cl_dio_aio *aio);
+
 static inline void cl_sync_io_init(struct cl_sync_io *anchor, int nr)
 {
 	cl_sync_io_init_notify(anchor, nr, NULL, NULL);

fs/lustre/llite/rw26.c
@@ -384,7 +384,7 @@ out:
 			vio->u.write.vui_written += tot_bytes;
 			result = tot_bytes;
 		}
-		kfree(aio);
+		cl_aio_free(aio);
 	} else {
 		result = -EIOCBQUEUED;
 	}

fs/lustre/obdclass/cl_io.c
@@ -1106,6 +1106,13 @@ struct cl_dio_aio *cl_aio_alloc(struct kiocb *iocb)
 }
 EXPORT_SYMBOL(cl_aio_alloc);
 
+void cl_aio_free(struct cl_dio_aio *aio)
+{
+	if (aio)
+		kmem_cache_free(cl_dio_aio_kmem, aio);
+}
+EXPORT_SYMBOL(cl_aio_free);
+
 /**
  * Indicate that transfer of a single page completed.
  */
@@ -1143,8 +1150,7 @@ void cl_sync_io_note(const struct lu_env *env, struct cl_sync_io *anchor,
 		 * If anchor->csi_aio is set, we are responsible for freeing
 		 * memory here rather than when cl_sync_io_wait() completes.
 		 */
-		if (aio)
-			kmem_cache_free(cl_dio_aio_kmem, aio);
+		cl_aio_free(aio);
 	}
 }
 EXPORT_SYMBOL(cl_sync_io_note);