jasimmons1973/lustre

commit fde7ac1942f57f45c0861d9b6b89e9bd9d0185ae

lustre: clio: AIO support for direct IO
This patch try to add aio support for Lustre, AIO is
doing IO like DIO but we don't wait IO finished upon
return, we return EIOCBQUEUED to vfs instead to indicate IO
have been issued, aio_complete() will be called in the
callback once IO have been done.

  fio AIO/DIO bandwidth results:
  # numjob=4, bs=512k

  MB/s      write       read
  master      832       1806
  patched    6591      11800

  fio AIO/DIO IOPS results:
  # 32 clients, 8192 threads
  # ioengine=libaio rw=randread blocksize=4096 iodepth=128 direct=1
  # size=1g runtime=300 group_reporting numjobs=256 create_serialize=0

  IOPS      write       read
  master      99K      1239K
  patched    265K      3498K

WC-bug-id: https://jira.whamcloud.com/browse/LU-4198
Lustre-commit: d1dded6e2847 ("LU-4198 clio: AIO support for direct IO")
Signed-off-by: Jinshan Xiong <jinshan.xiong@uber.com>
Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/32416
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/include/cl_object.h
@@ -2470,11 +2470,12 @@ void cl_req_attr_set(const struct lu_env *env, struct cl_object *obj,
  */
 
 struct cl_sync_io;
+struct cl_dio_aio;
 
 typedef void (cl_sync_io_end_t)(const struct lu_env *, struct cl_sync_io *);
 
 void cl_sync_io_init_notify(struct cl_sync_io *anchor, int nr,
-			    cl_sync_io_end_t *end);
+			    struct cl_dio_aio *aio, cl_sync_io_end_t *end);
 
 int cl_sync_io_wait(const struct lu_env *env, struct cl_sync_io *anchor,
 		    long timeout);
@@ -2482,7 +2483,7 @@ void cl_sync_io_note(const struct lu_env *env, struct cl_sync_io *anchor,
 		     int ioret);
 static inline void cl_sync_io_init(struct cl_sync_io *anchor, int nr)
 {
-	cl_sync_io_init_notify(anchor, nr, NULL);
+	cl_sync_io_init_notify(anchor, nr, NULL, NULL);
 }
 
 /**
@@ -2500,6 +2501,16 @@ struct cl_sync_io {
 	wait_queue_head_t	csi_waitq;
 	/** callback to invoke when this IO is finished */
 	cl_sync_io_end_t	*csi_end_io;
+	/** aio private data */
+	struct cl_dio_aio	*csi_aio;
+};
+
+/* To support Direct AIO */
+struct cl_dio_aio {
+	struct cl_sync_io	cda_sync;
+	struct cl_page_list	cda_pages;
+	struct kiocb		*cda_iocb;
+	ssize_t			cda_bytes;
 };
 
 /** @} cl_sync_io */

fs/lustre/llite/rw26.c
@@ -151,121 +151,160 @@ static int ll_releasepage(struct page *vmpage, gfp_t gfp_mask)
 	return result;
 }
 
-#define MAX_DIRECTIO_SIZE (2 * 1024 * 1024 * 1024UL)
-
-/*  ll_free_user_pages - tear down page struct array
- *  @pages: array of page struct pointers underlying target buffer
+/*
+ * ll_free_user_pages - tear down page struct array
+ * @pages: array of page struct pointers underlying target buffer
  */
-static void ll_free_user_pages(struct page **pages, int npages, int do_dirty)
+static void ll_free_user_pages(struct page **pages, int npages)
 {
 	int i;
 
 	for (i = 0; i < npages; i++) {
-		if (do_dirty)
-			set_page_dirty_lock(pages[i]);
+		if (!pages[i])
+			break;
 		put_page(pages[i]);
 	}
 	kvfree(pages);
 }
 
-static ssize_t ll_direct_IO_seg(const struct lu_env *env, struct cl_io *io,
-				int rw, struct inode *inode, size_t size,
-				loff_t file_offset, struct page **pages,
-				int page_count)
+static ssize_t ll_get_user_pages(int rw, struct iov_iter *iter,
+				struct page ***pages, ssize_t *npages,
+				size_t maxsize)
 {
-	struct cl_page *clp;
-	struct cl_2queue *queue;
+	size_t start;
+	size_t result;
+
+	result = iov_iter_get_pages_alloc(iter, pages, maxsize, &start);
+	if (result > 0)
+		*npages = DIV_ROUND_UP(result + start, PAGE_SIZE);
+
+	return result;
+}
+
+static inline void aio_complete(struct kiocb *iocb, ssize_t res, ssize_t res2)
+{
+	if (iocb->ki_complete)
+		iocb->ki_complete(iocb, res, res2);
+}
+
+/* direct IO pages */
+struct ll_dio_pages {
+	struct cl_dio_aio	*ldp_aio;
+	/*
+	 * page array to be written. we don't support
+	 * partial pages except the last one.
+	 */
+	struct page		**ldp_pages;
+	/* # of pages in the array. */
+	size_t			ldp_count;
+	/* the file offset of the first page. */
+	loff_t			ldp_file_offset;
+};
+
+void ll_aio_end(const struct lu_env *env, struct cl_sync_io *anchor)
+{
+	struct cl_dio_aio *aio = container_of(anchor, typeof(*aio), cda_sync);
+	ssize_t ret = anchor->csi_sync_rc;
+
+	/* release pages */
+	while (aio->cda_pages.pl_nr > 0) {
+		struct cl_page *page = cl_page_list_first(&aio->cda_pages);
+
+		cl_page_get(page);
+		cl_page_list_del(env, &aio->cda_pages, page);
+		cl_page_delete(env, page);
+		cl_page_put(env, page);
+	}
+
+	if (!is_sync_kiocb(aio->cda_iocb))
+		aio_complete(aio->cda_iocb, ret ?: aio->cda_bytes, 0);
+}
+
+static struct cl_dio_aio *ll_aio_alloc(struct kiocb *iocb)
+{
+	struct cl_dio_aio *aio;
+
+	aio = kzalloc(sizeof(*aio), GFP_NOFS);
+	if (aio) {
+		/*
+		 * Hold one ref so that it won't be released until
+		 * every pages is added.
+		 */
+		cl_sync_io_init_notify(&aio->cda_sync, 1, is_sync_kiocb(iocb) ?
+				       NULL : aio, ll_aio_end);
+		cl_page_list_init(&aio->cda_pages);
+		aio->cda_iocb = iocb;
+	}
+	return aio;
+}
+
+static int
+ll_direct_rw_pages(const struct lu_env *env, struct cl_io *io, size_t size,
+		   int rw, struct inode *inode, struct ll_dio_pages *pv)
+{
+	struct cl_page *page;
+	struct cl_2queue *queue = &io->ci_queue;
 	struct cl_object *obj = io->ci_obj;
+	struct cl_sync_io *anchor = &pv->ldp_aio->cda_sync;
+	loff_t offset = pv->ldp_file_offset;
+	int io_pages = 0;
+	size_t page_size = cl_page_size(obj);
 	int i;
 	ssize_t rc = 0;
-	size_t page_size = cl_page_size(obj);
-	size_t orig_size = size;
-	bool do_io;
-	int io_pages = 0;
 
-	queue = &io->ci_queue;
 	cl_2queue_init(queue);
-	for (i = 0; i < page_count; i++) {
-		LASSERT(!(file_offset & (page_size - 1)));
-		clp = cl_page_find(env, obj, cl_index(obj, file_offset),
-				   pages[i], CPT_TRANSIENT);
-		if (IS_ERR(clp)) {
-			rc = PTR_ERR(clp);
+	for (i = 0; i < pv->ldp_count; i++) {
+		LASSERT(!(offset & (PAGE_SIZE - 1)));
+		page = cl_page_find(env, obj, cl_index(obj, offset),
+				    pv->ldp_pages[i], CPT_TRANSIENT);
+		if (IS_ERR(page)) {
+			rc = PTR_ERR(page);
 			break;
 		}
-
-		rc = cl_page_own(env, io, clp);
+		LASSERT(page->cp_type == CPT_TRANSIENT);
+		rc = cl_page_own(env, io, page);
 		if (rc) {
-			LASSERT(clp->cp_state == CPS_FREEING);
-			cl_page_put(env, clp);
+			cl_page_put(env, page);
 			break;
 		}
 
-		do_io = true;
-
-		/* check the page type: if the page is a host page, then do
-		 * write directly
+		page->cp_sync_io = anchor;
+		cl_page_list_add(&queue->c2_qin, page);
+		/*
+		 * Set page clip to tell transfer formation engine
+		 * that page has to be sent even if it is beyond KMS.
 		 */
-		if (clp->cp_type == CPT_CACHEABLE) {
-			struct page *vmpage = cl_page_vmpage(clp);
-			struct page *src_page;
-			struct page *dst_page;
-			void *src;
-			void *dst;
-
-			src_page = (rw == WRITE) ? pages[i] : vmpage;
-			dst_page = (rw == WRITE) ? vmpage : pages[i];
-
-			src = kmap_atomic(src_page);
-			dst = kmap_atomic(dst_page);
-			memcpy(dst, src, min(page_size, size));
-			kunmap_atomic(dst);
-			kunmap_atomic(src);
-
-			/* make sure page will be added to the transfer by
-			 * cl_io_submit()->...->vvp_page_prep_write().
-			 */
-			if (rw == WRITE)
-				set_page_dirty(vmpage);
-
-			if (rw == READ) {
-				/* do not issue the page for read, since it
-				 * may reread a ra page which has NOT uptodate
-				 * bit set.
-				 */
-				cl_page_disown(env, io, clp);
-				do_io = false;
-			}
-		}
-
-		if (likely(do_io)) {
-			/*
-			 * Add a page to the incoming page list of 2-queue.
-			 */
-			cl_page_list_add(&queue->c2_qin, clp);
-
-			/*
-			 * Set page clip to tell transfer formation engine
-			 * that page has to be sent even if it is beyond KMS.
-			 */
-			cl_page_clip(env, clp, 0, min(size, page_size));
-
-			++io_pages;
-		}
+		cl_page_clip(env, page, 0, min(size, page_size));
+		++io_pages;
 
 		/* drop the reference count for cl_page_find */
-		cl_page_put(env, clp);
+		cl_page_put(env, page);
+		offset += page_size;
 		size -= page_size;
-		file_offset += page_size;
 	}
+	if (rc == 0 && io_pages > 0) {
+		int iot = rw == READ ? CRT_READ : CRT_WRITE;
 
-	if (rc == 0 && io_pages) {
-		rc = cl_io_submit_sync(env, io,
-				       rw == READ ? CRT_READ : CRT_WRITE,
-				       queue, 0);
+		atomic_add(io_pages, &anchor->csi_sync_nr);
+		rc = cl_io_submit_rw(env, io, iot, queue);
+		if (rc == 0) {
+			cl_page_list_splice(&queue->c2_qout,
+					&pv->ldp_aio->cda_pages);
+		} else {
+			atomic_add(-queue->c2_qin.pl_nr,
+				   &anchor->csi_sync_nr);
+			cl_page_list_for_each(page, &queue->c2_qin)
+				page->cp_sync_io = NULL;
+		}
+		/* handle partially submitted reqs */
+		if (queue->c2_qin.pl_nr > 0) {
+			CERROR(DFID " failed to submit %d dio pages: %zd\n",
+			       PFID(lu_object_fid(&obj->co_lu)),
+			       queue->c2_qin.pl_nr, rc);
+			if (rc == 0)
+				rc = -EIO;
+		}
 	}
-	if (rc == 0)
-		rc = orig_size;
 
 	cl_2queue_discard(env, io, queue);
 	cl_2queue_disown(env, io, queue);
@@ -279,8 +318,8 @@ static ssize_t ll_direct_IO_seg(const struct lu_env *env, struct cl_io *io,
  * then truncate this to be a full-sized RPC.  For 4kB PAGE_SIZE this is
  * up to 22MB for 128kB kmalloc and up to 682MB for 4MB kmalloc.
  */
-#define MAX_DIO_SIZE ((KMALLOC_MAX_SIZE / sizeof(struct brw_page) *	  \
-		       PAGE_SIZE) & ~(DT_MAX_BRW_SIZE - 1))
+#define MAX_DIO_SIZE ((KMALLOC_MAX_SIZE / sizeof(struct brw_page) * PAGE_SIZE) & \
+		      ~(DT_MAX_BRW_SIZE - 1))
 
 static ssize_t ll_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 {
@@ -289,13 +328,14 @@ static ssize_t ll_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	struct cl_io *io;
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = file->f_mapping->host;
-	loff_t file_offset = iocb->ki_pos;
-	ssize_t count = iov_iter_count(iter);
+	struct cl_dio_aio *aio;
+	size_t count = iov_iter_count(iter);
 	ssize_t tot_bytes = 0, result = 0;
-	long size = MAX_DIO_SIZE;
+	loff_t file_offset = iocb->ki_pos;
+	int rw = iov_iter_rw(iter);
 
 	/* Check EOF by ourselves */
-	if (iov_iter_rw(iter) == READ && file_offset >= i_size_read(inode))
+	if (rw == READ && file_offset >= i_size_read(inode))
 		return 0;
 
 	/* FIXME: io smaller than PAGE_SIZE is broken on ia64 ??? */
@@ -321,60 +361,77 @@ static ssize_t ll_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	io = lcc->lcc_io;
 	LASSERT(io);
 
-	while (iov_iter_count(iter)) {
-		struct page **pages;
-		size_t offs;
+	aio = ll_aio_alloc(iocb);
+	if (!aio)
+		return -ENOMEM;
 
-		count = min_t(size_t, iov_iter_count(iter), size);
-		if (iov_iter_rw(iter) == READ) {
+	/* 0. Need locking between buffered and direct access. and race with
+	 *    size changing by concurrent truncates and writes.
+	 * 1. Need inode mutex to operate transient pages.
+	 */
+	if (rw == READ)
+		inode_lock(inode);
+
+	while (iov_iter_count(iter)) {
+		struct ll_dio_pages pvec = { .ldp_aio = aio };
+		struct page **pages;
+
+		count = min_t(size_t, iov_iter_count(iter), MAX_DIO_SIZE);
+		if (rw == READ) {
 			if (file_offset >= i_size_read(inode))
 				break;
+
 			if (file_offset + count > i_size_read(inode))
 				count = i_size_read(inode) - file_offset;
 		}
 
-		result = iov_iter_get_pages_alloc(iter, &pages, count, &offs);
-		if (likely(result > 0)) {
-			int n = DIV_ROUND_UP(result + offs, PAGE_SIZE);
-
-			result = ll_direct_IO_seg(env, io, iov_iter_rw(iter),
-						  inode, result, file_offset,
-						  pages, n);
-			ll_free_user_pages(pages, n, iov_iter_rw(iter) == READ);
-		}
-		if (unlikely(result <= 0)) {
-			/* If we can't allocate a large enough buffer
-			 * for the request, shrink it to a smaller
-			 * PAGE_SIZE multiple and try again.
-			 * We should always be able to kmalloc for a
-			 * page worth of page pointers = 4MB on i386.
-			 */
-			if (result == -ENOMEM &&
-			    size > (PAGE_SIZE / sizeof(*pages)) *
-			    PAGE_SIZE) {
-				size = ((((size / 2) - 1) |
-					 ~PAGE_MASK) + 1) &
-					PAGE_MASK;
-				CDEBUG(D_VFSTRACE, "DIO size now %lu\n",
-				       size);
-				continue;
-			}
-
+		result = ll_get_user_pages(rw, iter, &pages,
+					   &pvec.ldp_count, count);
+		if (unlikely(result <= 0))
 			goto out;
-		}
-		iov_iter_advance(iter, result);
-		tot_bytes += result;
-		file_offset += result;
+
+		count = result;
+		pvec.ldp_file_offset = file_offset;
+		pvec.ldp_pages = pages;
+
+		result = ll_direct_rw_pages(env, io, count,
+					    rw, inode, &pvec);
+		ll_free_user_pages(pages, pvec.ldp_count);
+
+		if (unlikely(result < 0))
+			goto out;
+
+		iov_iter_advance(iter, count);
+		tot_bytes += count;
+		file_offset += count;
 	}
+
 out:
-	if (tot_bytes > 0) {
-		struct vvp_io *vio = vvp_env_io(env);
+	aio->cda_bytes = tot_bytes;
+	cl_sync_io_note(env, &aio->cda_sync, result);
 
-		/* no commit async for direct IO */
-		vio->u.write.vui_written += tot_bytes;
+	if (is_sync_kiocb(iocb)) {
+		ssize_t rc2;
+
+		rc2 = cl_sync_io_wait(env, &aio->cda_sync, 0);
+		if (result == 0 && rc2)
+			result = rc2;
+
+		if (result == 0) {
+			struct vvp_io *vio = vvp_env_io(env);
+			/* no commit async for direct IO */
+			vio->u.write.vui_written += tot_bytes;
+			result = tot_bytes;
+		}
+		kfree(aio);
+	} else {
+		result = -EIOCBQUEUED;
 	}
 
-	return tot_bytes ? tot_bytes : result;
+	if (rw == READ)
+		inode_unlock(inode);
+
+	return result;
 }
 
 /**

fs/lustre/llite/vvp_io.c
@@ -1149,7 +1149,7 @@ static int vvp_io_write_start(const struct lu_env *env,
 			inode_unlock(inode);
 
 		written = result;
-		if (result > 0 || result == -EIOCBQUEUED)
+		if (result > 0)
 			result = generic_write_sync(vio->vui_iocb, result);
 	}
 
@@ -1172,12 +1172,13 @@ static int vvp_io_write_start(const struct lu_env *env,
 		/* rewind ki_pos to where it has successfully committed */
 		vio->vui_iocb->ki_pos = pos + io->ci_nob - nob;
 	}
-	if (result > 0) {
+	if (result > 0 || result == -EIOCBQUEUED) {
 		set_bit(LLIF_DATA_MODIFIED, &(ll_i2info(inode))->lli_flags);
 
 		if (result < cnt)
 			io->ci_continue = 0;
-		result = 0;
+		if (result > 0)
+			result = 0;
 	}
 	return result;
 }

fs/lustre/obdclass/cl_io.c
@@ -1037,13 +1037,14 @@ EXPORT_SYMBOL(cl_req_attr_set);
  *		anchor->csi_waitq.lock
  */
 void cl_sync_io_init_notify(struct cl_sync_io *anchor, int nr,
-			    cl_sync_io_end_t *end)
+			    struct cl_dio_aio *aio, cl_sync_io_end_t *end)
 {
 	memset(anchor, 0, sizeof(*anchor));
 	init_waitqueue_head(&anchor->csi_waitq);
 	atomic_set(&anchor->csi_sync_nr, nr);
 	anchor->csi_sync_rc = 0;
 	anchor->csi_end_io = end;
+	anchor->csi_aio = aio;
 }
 EXPORT_SYMBOL(cl_sync_io_init_notify);
 
@@ -1097,6 +1098,8 @@ void cl_sync_io_note(const struct lu_env *env, struct cl_sync_io *anchor,
 	LASSERT(atomic_read(&anchor->csi_sync_nr) > 0);
 	if (atomic_dec_and_lock(&anchor->csi_sync_nr,
 				&anchor->csi_waitq.lock)) {
+		struct cl_dio_aio *aio = NULL;
+
 		cl_sync_io_end_t *end_io = anchor->csi_end_io;
 
 		/*
@@ -1107,9 +1110,16 @@ void cl_sync_io_note(const struct lu_env *env, struct cl_sync_io *anchor,
 		wake_up_all_locked(&anchor->csi_waitq);
 		if (end_io)
 			end_io(env, anchor);
+		if (anchor->csi_aio)
+			aio = anchor->csi_aio;
+
 		spin_unlock(&anchor->csi_waitq.lock);
 
-		/* Can't access anchor any more */
+		/**
+		 * If anchor->csi_aio is set, we are responsible for freeing
+		 * memory here rather than when cl_sync_io_wait() completes.
+		 */
+		kfree(aio);
 	}
 }
 EXPORT_SYMBOL(cl_sync_io_note);

fs/lustre/osc/osc_cache.c
@@ -1951,9 +1951,6 @@ static int try_to_add_extent_for_io(struct client_obd *cli,
 
 		if (!can_merge(ext, tmp))
 			return 0;
-
-		/* remove break for strict check */
-		break;
 	}
 
 	data->erd_max_extents--;

commit 52f2fc5bc5b70051f8884493ea5597e16517ab45

lustre: llite: fix short io for AIO
The problem is currently AIO could not handle i/o size > stripe size:

We need cl io loop to handle io across stripes, since -EIOCBQUEUED is
returned for AIO, io loop will be stopped thus short io happen.

The patch try to fix the problem by making IO engine aware of
special error, and it will be proceed to finish all IO requests.

Fixes: fde7ac1942f5 ("lustre: clio: AIO support for direct IO")
WC-bug-id: https://jira.whamcloud.com/browse/LU-13697
Lustre-commit: 84c3e85ced2dd ("LU-13697 llite: fix short io for AIO")
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/39104
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/include/cl_object.h
@@ -1814,6 +1814,8 @@ struct cl_io {
 	enum cl_io_state	ci_state;
 	/** main object this io is against. Immutable after creation. */
 	struct cl_object	*ci_obj;
+	/** one AIO request might be split in cl_io_loop */
+	struct cl_dio_aio	*ci_aio;
 	/**
 	 * Upper layer io, of which this io is a part of. Immutable after
 	 * creation.

fs/lustre/llite/file.c
@@ -1514,6 +1514,7 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 	int rc = 0;
 	unsigned int retried = 0;
 	unsigned int ignore_lockless = 0;
+	bool is_aio = false;
 
 	CDEBUG(D_VFSTRACE, "file: %pD, type: %d ppos: %llu, count: %zu\n",
 	       file, iot, *ppos, count);
@@ -1536,6 +1537,15 @@ restart:
 		vio->vui_fd  = file->private_data;
 		vio->vui_iter = args->u.normal.via_iter;
 		vio->vui_iocb = args->u.normal.via_iocb;
+		if (file->f_flags & O_DIRECT) {
+			if (!is_sync_kiocb(vio->vui_iocb))
+				is_aio = true;
+			io->ci_aio = cl_aio_alloc(vio->vui_iocb);
+			if (!io->ci_aio) {
+				rc = -ENOMEM;
+				goto out;
+			}
+		}
 		/*
 		 * Direct IO reads must also take range lock,
 		 * or multiple reads will try to work on the same pages
@@ -1567,7 +1577,14 @@ restart:
 		rc = io->ci_result;
 	}
 
-	if (io->ci_nob > 0) {
+	/*
+	 * In order to move forward AIO, ci_nob was increased,
+	 * but that doesn't mean io have been finished, it just
+	 * means io have been submited, we will always return
+	 * EIOCBQUEUED to the caller, So we could only return
+	 * number of bytes in non-AIO case.
+	 */
+	if (io->ci_nob > 0 && !is_aio) {
 		result += io->ci_nob;
 		count -= io->ci_nob;
 		*ppos = io->u.ci_wr.wr.crw_pos;
@@ -1577,6 +1594,19 @@ restart:
 			args->u.normal.via_iter = vio->vui_iter;
 	}
 out:
+	if (io->ci_aio) {
+		/**
+		 * Drop one extra reference so that end_io() could be
+		 * called for this IO context, we could call it after
+		 * we make sure all AIO requests have been proceed.
+		 */
+		cl_sync_io_note(env, &io->ci_aio->cda_sync,
+				rc == -EIOCBQUEUED ? 0 : rc);
+		if (!is_aio) {
+			cl_aio_free(io->ci_aio);
+			io->ci_aio = NULL;
+		}
+	}
 	cl_io_fini(env, io);
 
 	CDEBUG(D_VFSTRACE,

fs/lustre/llite/rw26.c
@@ -290,6 +290,7 @@ static ssize_t ll_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	ssize_t tot_bytes = 0, result = 0;
 	loff_t file_offset = iocb->ki_pos;
 	int rw = iov_iter_rw(iter);
+	struct vvp_io *vio;
 
 	/* if file is encrypted, return 0 so that we fall back to buffered IO */
 	if (IS_ENCRYPTED(inode))
@@ -319,12 +320,13 @@ static ssize_t ll_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 
 	env = lcc->lcc_env;
 	LASSERT(!IS_ERR(env));
+	vio = vvp_env_io(env);
 	io = lcc->lcc_io;
 	LASSERT(io);
 
-	aio = cl_aio_alloc(iocb);
-	if (!aio)
-		return -ENOMEM;
+	aio = io->ci_aio;
+	LASSERT(aio);
+	LASSERT(aio->cda_iocb == iocb);
 
 	/* 0. Need locking between buffered and direct access. and race with
 	 *    size changing by concurrent truncates and writes.
@@ -368,24 +370,39 @@ static ssize_t ll_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	}
 
 out:
-	aio->cda_bytes = tot_bytes;
-	cl_sync_io_note(env, &aio->cda_sync, result);
+	aio->cda_bytes += tot_bytes;
 
 	if (is_sync_kiocb(iocb)) {
+		struct cl_sync_io *anchor = &aio->cda_sync;
 		ssize_t rc2;
 
-		rc2 = cl_sync_io_wait(env, &aio->cda_sync, 0);
+		/**
+		 * @anchor was inited as 1 to prevent end_io to be
+		 * called before we add all pages for IO, so drop
+		 * one extra reference to make sure we could wait
+		 * count to be zero.
+		 */
+		cl_sync_io_note(env, anchor, result);
+
+		rc2 = cl_sync_io_wait(env, anchor, 0);
 		if (result == 0 && rc2)
 			result = rc2;
 
+		/**
+		 * One extra reference again, as if @anchor is
+		 * reused we assume it as 1 before using.
+		 */
+		atomic_add(1, &anchor->csi_sync_nr);
 		if (result == 0) {
-			struct vvp_io *vio = vvp_env_io(env);
 			/* no commit async for direct IO */
-			vio->u.write.vui_written += tot_bytes;
+			vio->u.readwrite.vui_written += tot_bytes;
 			result = tot_bytes;
 		}
-		cl_aio_free(aio);
 	} else {
+		if (rw == WRITE)
+			vio->u.readwrite.vui_written += tot_bytes;
+		else
+			vio->u.readwrite.vui_read += tot_bytes;
 		result = -EIOCBQUEUED;
 	}
 
@@ -523,7 +540,7 @@ again:
 	vmpage = grab_cache_page_nowait(mapping, index);
 	if (unlikely(!vmpage || PageDirty(vmpage) || PageWriteback(vmpage))) {
 		struct vvp_io *vio = vvp_env_io(env);
-		struct cl_page_list *plist = &vio->u.write.vui_queue;
+		struct cl_page_list *plist = &vio->u.readwrite.vui_queue;
 
 		/* if the page is already in dirty cache, we have to commit
 		 * the pages right now; otherwise, it may cause deadlock
@@ -685,17 +702,17 @@ static int ll_write_end(struct file *file, struct address_space *mapping,
 
 	LASSERT(cl_page_is_owned(page, io));
 	if (copied > 0) {
-		struct cl_page_list *plist = &vio->u.write.vui_queue;
+		struct cl_page_list *plist = &vio->u.readwrite.vui_queue;
 
 		lcc->lcc_page = NULL; /* page will be queued */
 
 		/* Add it into write queue */
 		cl_page_list_add(plist, page);
 		if (plist->pl_nr == 1) /* first page */
-			vio->u.write.vui_from = from;
+			vio->u.readwrite.vui_from = from;
 		else
 			LASSERT(from == 0);
-		vio->u.write.vui_to = from + copied;
+		vio->u.readwrite.vui_to = from + copied;
 
 		/*
 		 * To address the deadlock in balance_dirty_pages() where

fs/lustre/llite/vvp_internal.h
@@ -88,9 +88,10 @@ struct vvp_io {
 		struct {
 			struct cl_page_list	vui_queue;
 			unsigned long		vui_written;
+			unsigned long		vui_read;
 			int			vui_from;
 			int			vui_to;
-		} write;
+		} readwrite; /* normal io */
 	} u;
 
 	/**

fs/lustre/llite/vvp_io.c
@@ -249,10 +249,20 @@ static int vvp_io_write_iter_init(const struct lu_env *env,
 {
 	struct vvp_io *vio = cl2vvp_io(env, ios);
 
-	cl_page_list_init(&vio->u.write.vui_queue);
-	vio->u.write.vui_written = 0;
-	vio->u.write.vui_from = 0;
-	vio->u.write.vui_to = PAGE_SIZE;
+	cl_page_list_init(&vio->u.readwrite.vui_queue);
+	vio->u.readwrite.vui_written = 0;
+	vio->u.readwrite.vui_from = 0;
+	vio->u.readwrite.vui_to = PAGE_SIZE;
+
+	return 0;
+}
+
+static int vvp_io_read_iter_init(const struct lu_env *env,
+				 const struct cl_io_slice *ios)
+{
+	struct vvp_io *vio = cl2vvp_io(env, ios);
+
+	vio->u.readwrite.vui_read = 0;
 
 	return 0;
 }
@@ -262,7 +272,7 @@ static void vvp_io_write_iter_fini(const struct lu_env *env,
 {
 	struct vvp_io *vio = cl2vvp_io(env, ios);
 
-	LASSERT(vio->u.write.vui_queue.pl_nr == 0);
+	LASSERT(vio->u.readwrite.vui_queue.pl_nr == 0);
 }
 
 static int vvp_io_fault_iter_init(const struct lu_env *env,
@@ -824,7 +834,13 @@ out:
 			io->ci_continue = 0;
 		io->ci_nob += result;
 		result = 0;
+	} else if (result == -EIOCBQUEUED) {
+		io->ci_nob += vio->u.readwrite.vui_read;
+		if (vio->vui_iocb)
+			vio->vui_iocb->ki_pos = pos +
+						vio->u.readwrite.vui_read;
 	}
+
 	return result;
 }
 
@@ -1017,23 +1033,24 @@ int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io)
 	struct cl_object *obj = io->ci_obj;
 	struct inode *inode = vvp_object_inode(obj);
 	struct vvp_io *vio = vvp_env_io(env);
-	struct cl_page_list *queue = &vio->u.write.vui_queue;
+	struct cl_page_list *queue = &vio->u.readwrite.vui_queue;
 	struct cl_page *page;
 	int rc = 0;
 	int bytes = 0;
-	unsigned int npages = vio->u.write.vui_queue.pl_nr;
+	unsigned int npages = vio->u.readwrite.vui_queue.pl_nr;
 
 	if (npages == 0)
 		return 0;
 
 	CDEBUG(D_VFSTRACE, "commit async pages: %d, from %d, to %d\n",
-	       npages, vio->u.write.vui_from, vio->u.write.vui_to);
+	       npages, vio->u.readwrite.vui_from, vio->u.readwrite.vui_to);
 
 	LASSERT(page_list_sanity_check(obj, queue));
 
 	/* submit IO with async write */
 	rc = cl_io_commit_async(env, io, queue,
-				vio->u.write.vui_from, vio->u.write.vui_to,
+				vio->u.readwrite.vui_from,
+				vio->u.readwrite.vui_to,
 				write_commit_callback);
 	npages -= queue->pl_nr; /* already committed pages */
 	if (npages > 0) {
@@ -1041,18 +1058,18 @@ int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io)
 		bytes = npages << PAGE_SHIFT;
 
 		/* first page */
-		bytes -= vio->u.write.vui_from;
+		bytes -= vio->u.readwrite.vui_from;
 		if (queue->pl_nr == 0) /* last page */
-			bytes -= PAGE_SIZE - vio->u.write.vui_to;
+			bytes -= PAGE_SIZE - vio->u.readwrite.vui_to;
 		LASSERTF(bytes > 0, "bytes = %d, pages = %d\n", bytes, npages);
 
-		vio->u.write.vui_written += bytes;
+		vio->u.readwrite.vui_written += bytes;
 
 		CDEBUG(D_VFSTRACE, "Committed %d pages %d bytes, tot: %ld\n",
-		       npages, bytes, vio->u.write.vui_written);
+		       npages, bytes, vio->u.readwrite.vui_written);
 
 		/* the first page must have been written. */
-		vio->u.write.vui_from = 0;
+		vio->u.readwrite.vui_from = 0;
 	}
 	LASSERT(page_list_sanity_check(obj, queue));
 	LASSERT(ergo(rc == 0, queue->pl_nr == 0));
@@ -1060,10 +1077,10 @@ int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io)
 	/* out of quota, try sync write */
 	if (rc == -EDQUOT && !cl_io_is_mkwrite(io)) {
 		rc = vvp_io_commit_sync(env, io, queue,
-					vio->u.write.vui_from,
-					vio->u.write.vui_to);
+					vio->u.readwrite.vui_from,
+					vio->u.readwrite.vui_to);
 		if (rc > 0) {
-			vio->u.write.vui_written += rc;
+			vio->u.readwrite.vui_written += rc;
 			rc = 0;
 		}
 	}
@@ -1181,15 +1198,15 @@ static int vvp_io_write_start(const struct lu_env *env,
 		result = vvp_io_write_commit(env, io);
 		/* Simulate short commit */
 		if (CFS_FAULT_CHECK(OBD_FAIL_LLITE_SHORT_COMMIT)) {
-			vio->u.write.vui_written >>= 1;
-			if (vio->u.write.vui_written > 0)
+			vio->u.readwrite.vui_written >>= 1;
+			if (vio->u.readwrite.vui_written > 0)
 				io->ci_need_restart = 1;
 		}
-		if (vio->u.write.vui_written > 0) {
-			result = vio->u.write.vui_written;
+		if (vio->u.readwrite.vui_written > 0) {
+			result = vio->u.readwrite.vui_written;
 			io->ci_nob += result;
-
-			CDEBUG(D_VFSTRACE, "write: nob %zd, result: %zd\n",
+			CDEBUG(D_VFSTRACE, "%s: write: nob %zd, result: %zd\n",
+			       file_dentry(file)->d_name.name,
 			       io->ci_nob, result);
 		} else {
 			io->ci_continue = 0;
@@ -1215,11 +1232,18 @@ static int vvp_io_write_start(const struct lu_env *env,
 	if (result > 0 || result == -EIOCBQUEUED) {
 		set_bit(LLIF_DATA_MODIFIED, &(ll_i2info(inode))->lli_flags);
 
-		if (result < cnt)
+		if (result != -EIOCBQUEUED && result < cnt)
 			io->ci_continue = 0;
 		if (result > 0)
 			result = 0;
+		/* move forward */
+		if (result == -EIOCBQUEUED) {
+			io->ci_nob += vio->u.readwrite.vui_written;
+			vio->vui_iocb->ki_pos = pos +
+						vio->u.readwrite.vui_written;
+		}
 	}
+
 	return result;
 }
 
@@ -1509,6 +1533,7 @@ static const struct cl_io_operations vvp_io_ops = {
 	.op = {
 		[CIT_READ] = {
 			.cio_fini	= vvp_io_fini,
+			.cio_iter_init	= vvp_io_read_iter_init,
 			.cio_lock	= vvp_io_read_lock,
 			.cio_start	= vvp_io_read_start,
 			.cio_end	= vvp_io_rw_end,

fs/lustre/obdclass/cl_io.c
@@ -695,6 +695,7 @@ EXPORT_SYMBOL(cl_io_submit_sync);
 int cl_io_loop(const struct lu_env *env, struct cl_io *io)
 {
 	int result = 0;
+	int rc = 0;
 
 	LINVRNT(cl_io_is_loopable(io));
 
@@ -727,7 +728,13 @@ int cl_io_loop(const struct lu_env *env, struct cl_io *io)
 			}
 		}
 		cl_io_iter_fini(env, io);
-	} while (result == 0 && io->ci_continue);
+		if (result)
+			rc = result;
+	} while ((result == 0 || result == -EIOCBQUEUED) &&
+		 io->ci_continue);
+
+	if (rc && !result)
+		result = rc;
 
 	if (result == -EWOULDBLOCK && io->ci_ndelay) {
 		io->ci_need_restart = 1;