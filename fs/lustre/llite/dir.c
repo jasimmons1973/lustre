eefa033210e224cf278f8019ac6bf2f1148815ad
Lai Siyao <lai.siyao@whamcloud.com>
2021-04-17 12:54:10 -0400

    lustre: mdt: mkdir should return -EEXIST if exists
    lfs setdirstripe' will try restripe if target exists, however
    it's confusing to get -ENOTSUPP or -EALREADY for 'lfs mkdir', while
    the latter invokes the same function as 'lfs setdirstripe'.
    
    Pack MDS_OPEN_CREAT flag in request for 'lfs mkdir', and MDT won't
    try restripe if it's set.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-14366
    Lustre-commit: 65e3e4050ec5bb371 ("LU-14366 mdt: lfs mkdir should return -EEXIST if exists")
    Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/41329
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Yingjin Qian <qian@ddn.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/llite/dir.c
+++ fs/lustre/llite/dir.c
@@ -373,7 +373,8 @@ out:
  *		<0 if the creation is failed.
  */
 static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
-			       size_t len, const char *dirname, umode_t mode)
+			       size_t len, const char *dirname, umode_t mode,
+			       bool createonly)
 {
 	struct inode *parent = dparent->d_inode;
 	struct ptlrpc_request *request = NULL;
@@ -482,6 +483,9 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 	}
 
 	op_data->op_cli_flags |= CLI_SET_MEA;
+	if (createonly)
+		op_data->op_bias |= MDS_SETSTRIPE_CREATE;
+
 	err = md_create(sbi->ll_md_exp, op_data, lump, len, mode,
 			from_kuid(&init_user_ns, current_fsuid()),
 			from_kgid(&init_user_ns, current_fsgid()),
@@ -1378,6 +1382,7 @@ out_free:
 		int namelen = 0;
 		int lumlen = 0;
 		umode_t mode;
+		bool createonly = false;
 		int len;
 		int rc;
 
@@ -1427,7 +1432,9 @@ out_free:
 		}
 
 		mode = data->ioc_type;
-		rc = ll_dir_setdirstripe(dentry, lum, lumlen, filename, mode);
+		createonly = data->ioc_obdo1.o_flags & OBD_FL_OBDMDEXISTS;
+		rc = ll_dir_setdirstripe(dentry, lum, lumlen, filename, mode,
+					 createonly);
 lmv_out_free:
 		kvfree(data);
 		return rc;
