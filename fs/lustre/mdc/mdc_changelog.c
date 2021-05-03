jasimmons1973/lustre

commit dcedf3009a7177a17cf1e755043971d3d58f69ab

lustre: changelog: support large number of MDT
At client, the changelog of each MDT is associated with
one miscdevice, but the number of miscdevice is limited
to 64 in Linux kernel, then it will fail if there are
more than 64 MDTs.

This patch replaces miscdevice with dynamic devices to
support more MDTs.

WC-bug-id: https://jira.whamcloud.com/browse/LU-12506
Lustre-commit: d0423abc1adc7 ("LU-12506 changelog: support large number of MDT")
Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/37759
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradaed.org>

fs/lustre/mdc/mdc_changelog.c
@@ -34,7 +34,8 @@
 #include <linux/init.h>
 #include <linux/kthread.h>
 #include <linux/poll.h>
-#include <linux/miscdevice.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 
 #include <lustre_log.h>
 #include <uapi/linux/lustre/lustre_ioctl.h>
@@ -58,8 +59,9 @@ static LIST_HEAD(chlg_registered_devices);
 struct chlg_registered_dev {
 	/* Device name of the form "changelog-{MDTNAME}" */
 	char			ced_name[32];
-	/* Misc device descriptor */
-	struct miscdevice	ced_misc;
+	/* changelog char device */
+	struct cdev		ced_cdev;
+	struct device		*ced_device;
 	/* OBDs referencing this device (multiple mount point) */
 	struct list_head	ced_obds;
 	/* Reference counter for proper deregistration */
@@ -110,18 +112,56 @@ enum {
 	CDEV_CHLG_MAX_PREFETCH = 1024,
 };
 
+static DEFINE_IDR(chlg_minor_idr);
+static DEFINE_SPINLOCK(chlg_minor_lock);
+
+static int chlg_minor_alloc(int *pminor)
+{
+	void *minor_allocated = (void *)-1;
+	int minor;
+
+	idr_preload(GFP_KERNEL);
+	spin_lock(&chlg_minor_lock);
+	minor = idr_alloc(&chlg_minor_idr, minor_allocated, 0,
+			  MDC_CHANGELOG_DEV_COUNT, GFP_NOWAIT);
+	spin_unlock(&chlg_minor_lock);
+	idr_preload_end();
+
+	if (minor < 0)
+		return minor;
+
+	*pminor = minor;
+	return 0;
+}
+
+static void chlg_minor_free(int minor)
+{
+	spin_lock(&chlg_minor_lock);
+	idr_remove(&chlg_minor_idr, minor);
+	spin_unlock(&chlg_minor_lock);
+}
+
+static void chlg_device_release(struct device *dev)
+{
+	struct chlg_registered_dev *entry = dev_get_drvdata(dev);
+
+	chlg_minor_free(MINOR(entry->ced_cdev.dev));
+	kfree(entry);
+}
+
 /**
  * Deregister a changelog character device whose refcount has reached zero.
  */
 static void chlg_dev_clear(struct kref *kref)
 {
-	struct chlg_registered_dev *entry = container_of(kref,
-						struct chlg_registered_dev,
-						ced_refs);
+	struct chlg_registered_dev *entry;
+
+	entry = container_of(kref, struct chlg_registered_dev,
+			     ced_refs);
 
 	list_del(&entry->ced_link);
-	misc_deregister(&entry->ced_misc);
-	kfree(entry);
+	cdev_del(&entry->ced_cdev);
+	device_destroy(mdc_changelog_class, entry->ced_cdev.dev);
 }
 
 static inline struct obd_device *chlg_obd_get(struct chlg_registered_dev *dev)
@@ -559,12 +599,11 @@ out_kbuf:
 static int chlg_open(struct inode *inode, struct file *file)
 {
 	struct chlg_reader_state *crs;
-	struct miscdevice *misc = file->private_data;
 	struct chlg_registered_dev *dev;
 	struct task_struct *task;
 	int rc;
 
-	dev = container_of(misc, struct chlg_registered_dev, ced_misc);
+	dev = container_of(inode->i_cdev, struct chlg_registered_dev, ced_cdev);
 
 	crs = kzalloc(sizeof(*crs), GFP_KERNEL);
 	if (!crs)
@@ -684,11 +723,11 @@ static const struct file_operations chlg_fops = {
  * This uses obd_name of the form: "testfs-MDT0000-mdc-ffff88006501600"
  * and returns a name of the form: "changelog-testfs-MDT0000".
  */
-static void get_chlg_name(char *name, size_t name_len, struct obd_device *obd)
+static void get_target_name(char *name, size_t name_len, struct obd_device *obd)
 {
 	int i;
 
-	snprintf(name, name_len, "changelog-%s", obd->obd_name);
+	snprintf(name, name_len, "%s", obd->obd_name);
 
 	/* Find the 2nd '-' from the end and truncate on it */
 	for (i = 0; i < 2; i++) {
@@ -751,17 +790,15 @@ int mdc_changelog_cdev_init(struct obd_device *obd)
 {
 	struct chlg_registered_dev *exist;
 	struct chlg_registered_dev *entry;
-	int rc;
+	struct device *device;
+	dev_t dev;
+	int minor, rc;
 
 	entry = kzalloc(sizeof(*entry), GFP_KERNEL);
 	if (!entry)
 		return -ENOMEM;
 
-	get_chlg_name(entry->ced_name, sizeof(entry->ced_name), obd);
-
-	entry->ced_misc.minor = MISC_DYNAMIC_MINOR;
-	entry->ced_misc.name  = entry->ced_name;
-	entry->ced_misc.fops  = &chlg_fops;
+	get_target_name(entry->ced_name, sizeof(entry->ced_name), obd);
 
 	kref_init(&entry->ced_refs);
 	INIT_LIST_HEAD(&entry->ced_obds);
@@ -780,14 +817,39 @@ int mdc_changelog_cdev_init(struct obd_device *obd)
 	list_add_tail(&entry->ced_link, &chlg_registered_devices);
 
 	/* Register new character device */
-	rc = misc_register(&entry->ced_misc);
-	if (rc != 0) {
-		list_del_init(&obd->u.cli.cl_chg_dev_linkage);
-		list_del(&entry->ced_link);
+	cdev_init(&entry->ced_cdev, &chlg_fops);
+	entry->ced_cdev.owner = THIS_MODULE;
+
+	rc = chlg_minor_alloc(&minor);
+	if (rc)
 		goto out_unlock;
+
+	dev = MKDEV(MAJOR(mdc_changelog_dev), minor);
+	rc = cdev_add(&entry->ced_cdev, dev, 1);
+	if (rc)
+		goto out_minor;
+
+	device = device_create(mdc_changelog_class, NULL, dev, entry, "%s-%s",
+			       MDC_CHANGELOG_DEV_NAME, entry->ced_name);
+	if (IS_ERR(device)) {
+		rc = PTR_ERR(device);
+		goto out_cdev;
 	}
 
+	device->release = chlg_device_release;
+	entry->ced_device = device;
+
 	entry = NULL;	/* prevent it from being freed below */
+	goto out_unlock;
+
+out_cdev:
+	cdev_del(&entry->ced_cdev);
+
+out_minor:
+	chlg_minor_free(minor);
+
+	list_del_init(&obd->u.cli.cl_chg_dev_linkage);
+	list_del(&entry->ced_link);
 
 out_unlock:
 	mutex_unlock(&chlg_registered_dev_lock);

fs/lustre/mdc/mdc_internal.h
@@ -138,6 +138,11 @@ enum ldlm_mode mdc_lock_match(struct obd_export *exp, u64 flags,
 			      enum ldlm_mode mode,
 			      struct lustre_handle *lockh);
 
+#define MDC_CHANGELOG_DEV_COUNT LMV_MAX_STRIPE_COUNT
+#define MDC_CHANGELOG_DEV_NAME "changelog"
+extern struct class *mdc_changelog_class;
+extern dev_t mdc_changelog_dev;
+
 int mdc_changelog_cdev_init(struct obd_device *obd);
 
 void mdc_changelog_cdev_finish(struct obd_device *obd);

fs/lustre/mdc/mdc_request.c
@@ -41,6 +41,7 @@
 #include <linux/file.h>
 #include <linux/kthread.h>
 #include <linux/prefetch.h>
+#include <linux/device.h>
 
 #include <lustre_errno.h>
 #include <cl_object.h>
@@ -2969,20 +2970,45 @@ static const struct md_ops mdc_md_ops = {
 	.rmfid			= mdc_rmfid,
 };
 
+dev_t mdc_changelog_dev;
+struct class *mdc_changelog_class;
+
 static int __init mdc_init(void)
 {
-	int rc;
+	int rc = 0;
 
 	rc = libcfs_setup();
 	if (rc)
 		return rc;
 
-	return class_register_type(&mdc_obd_ops, &mdc_md_ops,
-				   LUSTRE_MDC_NAME, &mdc_device_type);
+	rc = alloc_chrdev_region(&mdc_changelog_dev, 0,
+				 MDC_CHANGELOG_DEV_COUNT,
+				 MDC_CHANGELOG_DEV_NAME);
+	if (rc)
+		return rc;
+
+	mdc_changelog_class = class_create(THIS_MODULE, MDC_CHANGELOG_DEV_NAME);
+	if (IS_ERR(mdc_changelog_class)) {
+		rc = PTR_ERR(mdc_changelog_class);
+		goto out_dev;
+	}
+
+	rc = class_register_type(&mdc_obd_ops, &mdc_md_ops,
+				 LUSTRE_MDC_NAME, &mdc_device_type);
+	if (rc)
+		goto out_dev;
+
+	return 0;
+
+out_dev:
+	unregister_chrdev_region(mdc_changelog_dev, MDC_CHANGELOG_DEV_COUNT);
+	return rc;
 }
 
-static void /*__exit*/ mdc_exit(void)
+static void __exit mdc_exit(void)
 {
+	class_destroy(mdc_changelog_class);
+	unregister_chrdev_region(mdc_changelog_dev, MDC_CHANGELOG_DEV_COUNT);
 	class_unregister_type(LUSTRE_MDC_NAME);
 }
 

commit 021259f3c1dffc3f3a610fa0bc0ebd83f8b80886

lustre: mdc: include linux/idr.h for referenced code
Include the <linux/idr.h> header in files that references IDR
functionality.  Don't depend on its indirect inclusion elsewhere.

Fixes: dcedf3009a71 ("lustre: changelog: support large number of MDT")
WC-bug-id: https://jira.whamcloud.com/browse/LU-6142
Lustre-commit: 3589a3141a4b9f94 ("LU-6142 mdc: include linux/idr.h for referenced code")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/43346
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/mdc/mdc_changelog.c
@@ -36,6 +36,7 @@
 #include <linux/poll.h>
 #include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/idr.h>
 
 #include <lustre_log.h>
 #include <uapi/linux/lustre/lustre_ioctl.h>