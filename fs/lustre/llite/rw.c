jasimmons1973/lustre

commit 3b1dfe4b4bc6232f813c863bd4361845edaeab3b

lustre: llite: fix to submit complete read block with ra disabled
Even if readahead disabled, we need to make first cache-miss read
cover current read. Otherwise, without readahead, we are
always sending 1 page RPC which makes performances really bad.

Benchmarked with following command:

iozone -w -c -i 5 -t1 -j 2 -s 100m -r 1m data

Without patch: 39917.20 kB/sec
                            read                    write
pages per rpc         rpcs   % cum % |       rpcs   % cum %
1:                   25088 100 100   |          0   0   0

With patch: 754811.62 kB/sec
                            read                    write
pages per rpc         rpcs   % cum % |       rpcs   % cum %
1:                       0   0   0   |          0   0   0
2:                       0   0   0   |          0   0   0
4:                       0   0   0   |          0   0   0
8:                       0   0   0   |          0   0   0
16:                      0   0   0   |          0   0   0
32:                      0   0   0   |          0   0   0
64:                      0   0   0   |          0   0   0
128:                     0   0   0   |          0   0   0
256:                    98 100 100   |          0   0   0

We got huge peformances up x17 performances up, this is really
expected behavior without RA since we don't readahead any extra
pages, we just send RPC with one transfer size once rather than
send page by page, this help several cases that i could think of:

1) make more sense to compare RA without RA performances, for example
   overhead of RA.

2) we could get better performances with multiple thread read same
   file without RA.

Same test with RA enabled: 737746.56 kB/sec
                            read                    write
pages per rpc         rpcs   % cum % |       rpcs   % cum %
1:                       0   0   0   |          0   0   0
2:                       0   0   0   |          0   0   0
4:                       0   0   0   |          0   0   0

fs/lustre/llite/rw.c
@@ -123,6 +123,12 @@ static void ll_ra_stats_inc_sbi(struct ll_sb_info *sbi, enum ra_stat which)
 	lprocfs_counter_incr(sbi->ll_ra_stats, which);
 }
 
+static inline bool ll_readahead_enabled(struct ll_sb_info *sbi)
+{
+	return sbi->ll_ra_info.ra_max_pages_per_file > 0 &&
+	       sbi->ll_ra_info.ra_max_pages > 0;
+}
+
 void ll_ra_stats_inc(struct inode *inode, enum ra_stat which)
 {
 	struct ll_sb_info *sbi = ll_i2sbi(inode);
@@ -155,6 +161,11 @@ static bool pos_in_window(loff_t pos, loff_t point,
 	return start <= pos && pos <= end;
 }
 
+enum ll_ra_page_hint {
+	MAYNEED = 0,	/* this page possibly accessed soon */
+	WILLNEED	/* this page is gurateed to be needed */
+};
+
 /**
  * Initiates read-ahead of a page with given index.
  *
@@ -164,7 +175,8 @@ static bool pos_in_window(loff_t pos, loff_t point,
  *		0 if page was added into @queue for read ahead.
  */
 static int ll_read_ahead_page(const struct lu_env *env, struct cl_io *io,
-			      struct cl_page_list *queue, pgoff_t index)
+			      struct cl_page_list *queue, pgoff_t index,
+			      enum ll_ra_page_hint hint)
 {
 	enum ra_stat which = _NR_RA_STAT; /* keep gcc happy */
 	struct cl_object *clob = io->ci_obj;
@@ -172,14 +184,30 @@ static int ll_read_ahead_page(const struct lu_env *env, struct cl_io *io,
 	const char *msg = NULL;
 	struct cl_page *page;
 	struct vvp_page *vpg;
-	struct page *vmpage;
+	struct page *vmpage = NULL;
 	int rc = 0;
 
-	vmpage = grab_cache_page_nowait(inode->i_mapping, index);
-	if (!vmpage) {
-		which = RA_STAT_FAILED_GRAB_PAGE;
-		msg = "g_c_p_n failed";
-		rc = -EBUSY;
+	switch (hint) {
+	case MAYNEED:
+		vmpage = grab_cache_page_nowait(inode->i_mapping, index);
+		if (!vmpage) {
+			which = RA_STAT_FAILED_GRAB_PAGE;
+			msg = "g_c_p_n failed";
+			rc = -EBUSY;
+			goto out;
+		}
+		break;
+	case WILLNEED:
+		vmpage = find_or_create_page(inode->i_mapping, index,
+					     GFP_NOFS);
+		if (!vmpage) {
+			rc = -ENOMEM;
+			goto out;
+		}
+		break;
+	default:
+		/* should not come here */
+		rc = -EINVAL;
 		goto out;
 	}
 
@@ -223,7 +251,7 @@ out:
 			unlock_page(vmpage);
 		put_page(vmpage);
 	}
-	if (msg) {
+	if (msg && hint == MAYNEED) {
 		ll_ra_stats_inc(inode, which);
 		CDEBUG(D_READA, "%s\n", msg);
 	}
@@ -426,7 +454,8 @@ ll_read_ahead_pages(const struct lu_env *env, struct cl_io *io,
 				break;
 
 			/* If the page is inside the read-ahead window */
-			rc = ll_read_ahead_page(env, io, queue, page_idx);
+			rc = ll_read_ahead_page(env, io, queue, page_idx,
+						MAYNEED);
 			if (rc < 0 && rc != -EBUSY)
 				break;
 			if (rc == -EBUSY) {
@@ -780,6 +809,42 @@ static int ll_readahead(const struct lu_env *env, struct cl_io *io,
 	}
 
 	return ret;
+}
+
+static int ll_readpages(const struct lu_env *env, struct cl_io *io,
+			struct cl_page_list *queue,
+			pgoff_t start, pgoff_t end)
+{
+	int ret = 0;
+	u64 kms;
+	pgoff_t page_idx;
+	int count = 0;
+
+	ret = ll_readahead_file_kms(env, io, &kms);
+	if (ret)
+		return ret;
+
+	if (kms == 0)
+		return 0;
+
+	if (end != 0) {
+		unsigned long end_index;
+
+		end_index = (unsigned long)((kms - 1) >> PAGE_SHIFT);
+		if (end_index <= end)
+			end = end_index;
+	}
+
+	for (page_idx = start; page_idx <= end; page_idx++) {
+		ret = ll_read_ahead_page(env, io, queue, page_idx,
+					 WILLNEED);
+		if (ret < 0)
+			break;
+		else if (ret == 0) /* ret 1 is already uptodate */
+			count++;
+	}
+
+	return count > 0 ? count : ret;
 }
 
 static void ras_set_start(struct ll_readahead_state *ras, pgoff_t index)
@@ -1383,16 +1448,16 @@ int ll_io_read_page(const struct lu_env *env, struct cl_io *io,
 	struct cl_2queue *queue = &io->ci_queue;
 	struct ll_sb_info *sbi = ll_i2sbi(inode);
 	struct cl_sync_io *anchor = NULL;
+	pgoff_t io_start_index;
+	pgoff_t io_end_index;
+	int rc = 0, rc2 = 0;
 	struct vvp_page *vpg;
 	bool uptodate;
-	int rc = 0;
 
 	vpg = cl2vvp_page(cl_object_page_slice(page->cp_obj, page));
 	uptodate = vpg->vpg_defer_uptodate;
 
-	if (sbi->ll_ra_info.ra_max_pages_per_file > 0 &&
-	    sbi->ll_ra_info.ra_max_pages > 0 &&
-	    !vpg->vpg_ra_updated) {
+	if (ll_readahead_enabled(sbi) && !vpg->vpg_ra_updated) {
 		struct vvp_io *vio = vvp_env_io(env);
 		enum ras_update_flags flags = 0;
 
@@ -1416,13 +1481,19 @@ int ll_io_read_page(const struct lu_env *env, struct cl_io *io,
 		cl_page_list_add(&queue->c2_qin, page);
 	}
 
-	if (sbi->ll_ra_info.ra_max_pages_per_file > 0 &&
-	    sbi->ll_ra_info.ra_max_pages > 0) {
-		int rc2;
-
+	io_start_index = cl_index(io->ci_obj, io->u.ci_rw.crw_pos);
+	io_end_index = cl_index(io->ci_obj, io->u.ci_rw.crw_pos +
+				io->u.ci_rw.crw_count - 1);
+	if (ll_readahead_enabled(sbi)) {
 		rc2 = ll_readahead(env, io, &queue->c2_qin, ras,
 				   uptodate, file);
-		CDEBUG(D_READA, DFID "%d pages read ahead at %lu\n",
+		CDEBUG(D_READA, DFID " %d pages read ahead at %lu\n",
+		       PFID(ll_inode2fid(inode)), rc2, vvp_index(vpg));
+	} else if (vvp_index(vpg) == io_start_index &&
+		   io_end_index - io_start_index > 0) {
+		rc2 = ll_readpages(env, io, &queue->c2_qin, io_start_index + 1,
+				   io_end_index);
+		CDEBUG(D_READA, DFID " %d pages read at %lu\n",
 		       PFID(ll_inode2fid(inode)), rc2, vvp_index(vpg));
 	}
 

commit 8706195ba6ef759c7d54c1238b7cad8c91bc1dc2

lustre: readahead: export pages directly without RA
With Readahead disabled, @vpg_defer_uptodate should not
be set as we don't reserve credits for such read.
In vvp_page_completion_read() we will call ll_ra_count_put()
which makes @ra_cur_pages negative.

Fixes: 3b1dfe4b4b ("lustre: llite: fix to submit complete read block with ra disabled")
WC-bug-id: https://jira.whamcloud.com/browse/LU-14616
Lustre-commit: 9f1c0bfd10d619a3 ("LU-14616 readahead: export pages directly without RA")
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/43338
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/llite/rw.c
@@ -237,8 +237,10 @@ static int ll_read_ahead_page(const struct lu_env *env, struct cl_io *io,
 	cl_page_assume(env, io, page);
 	vpg = cl2vvp_page(cl_object_page_slice(clob, page));
 	if (!vpg->vpg_defer_uptodate && !PageUptodate(vmpage)) {
-		vpg->vpg_defer_uptodate = 1;
-		vpg->vpg_ra_used = 0;
+		if (hint == MAYNEED) {
+			vpg->vpg_defer_uptodate = 1;
+			vpg->vpg_ra_used = 0;
+		}
 		cl_page_list_add(queue, page);
 	} else {
 		/* skip completed pages */