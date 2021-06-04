9c7625071243761b6ea6308cd884b92ec5f86ed2
Patrick Farrell <farr0186@gmail.com>
2021-06-04 09:28:51 -0400

    lustre: llite: Return errors for aio
    The aio code incorrectly discards errors from
    ll_direct_rw_pages.  Fix this and add a test for this.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-14687
    Lustre-commit: 3e1f8d30cb0209b3 ("LU-14687 llite: Return errors for aio")
    Signed-off-by: Patrick Farrell <farr0186@gmail.com>
    Reviewed-on: https://review.whamcloud.com/43722
    Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
    Reviewed-by: Wang Shilong <wshilong@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/llite/rw26.c
+++ fs/lustre/llite/rw26.c
@@ -435,7 +435,8 @@ out:
 			vio->u.readwrite.vui_written += tot_bytes;
 		else
 			vio->u.readwrite.vui_read += tot_bytes;
-		result = -EIOCBQUEUED;
+		if (result == 0)
+			result = -EIOCBQUEUED;
 	}
 
 	return result;
