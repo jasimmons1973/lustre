47f7cf651db5beeee30178de3e705f0da8411241
Oleg Drokin <green@whamcloud.com>
2021-06-12 21:48:01 -0400

    lustre: update version to 2.14.52
    New tag 2.14.52
    
    Signed-off-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- include/uapi/linux/lustre/lustre_ver.h
+++ include/uapi/linux/lustre/lustre_ver.h
@@ -3,9 +3,9 @@
 
 #define LUSTRE_MAJOR 2
 #define LUSTRE_MINOR 14
-#define LUSTRE_PATCH 51
+#define LUSTRE_PATCH 52
 #define LUSTRE_FIX 0
-#define LUSTRE_VERSION_STRING "2.14.51"
+#define LUSTRE_VERSION_STRING "2.14.52"
 
 #define OBD_OCD_VERSION(major, minor, patch, fix)			\
 	(((major) << 24) + ((minor) << 16) + ((patch) << 8) + (fix))
