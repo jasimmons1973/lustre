2c46a805bc51fd4bcb5506e085a181353530c9a9
Andrew Perepechko <andrew.perepechko@hpe.com>
2021-11-06 11:41:59 -0400

    lustre: vfs: set_nlink() is not race-safe
    set_nlink() is not atomic wrt race with itself and
    the following warning may be triggered by VFS:
    
    WARNING: CPU: 5 PID: 195090 at fs/inode.c:241 __destroy_inode+0xdb/0xf0
    
    It does not seem important what exact nlink value is the result
    of the race. However, we need to protect the superblock remove
    counter.
    
    HPE-bug-id: LUS-9825
    WC-bug-id: https://jira.whamcloud.com/browse/LU-15081
    Lustre-commit: 12b05772fdb6d0808 ("LU-15081 vfs: set_nlink() is not race-safe")
    Signed-off-by: Andrew Perepechko <andrew.perepechko@hpe.com>
    Reviewed-on: https://review.whamcloud.com/45191
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/llite/namei.c
+++ fs/lustre/llite/namei.c
@@ -1867,8 +1867,11 @@ static int ll_unlink(struct inode *dir, struct dentry *dchild)
 	 * the link count so the inode can be freed immediately.
 	 */
 	body = req_capsule_server_get(&request->rq_pill, &RMF_MDT_BODY);
-	if (body->mbo_valid & OBD_MD_FLNLINK)
+	if (body->mbo_valid & OBD_MD_FLNLINK) {
+		spin_lock(&dchild->d_inode->i_lock);
 		set_nlink(dchild->d_inode, body->mbo_nlink);
+		spin_unlock(&dchild->d_inode->i_lock);
+	}
 
 	ll_update_times(request, dir);
 	ll_stats_ops_tally(ll_i2sbi(dir), LPROC_LL_UNLINK,
@@ -1938,8 +1941,11 @@ static int ll_rmdir(struct inode *dir, struct dentry *dchild)
 		 * immediately.
 		 */
 		body = req_capsule_server_get(&request->rq_pill, &RMF_MDT_BODY);
-		if (body->mbo_valid & OBD_MD_FLNLINK)
+		if (body->mbo_valid & OBD_MD_FLNLINK) {
+			spin_lock(&dchild->d_inode->i_lock);
 			set_nlink(dchild->d_inode, body->mbo_nlink);
+			spin_unlock(&dchild->d_inode->i_lock);
+		}
 	}
 
 	ptlrpc_req_finished(request);
