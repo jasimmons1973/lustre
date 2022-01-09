a8948860e43543f3aae226c3560bf5439eb35d11
Lai Siyao <lai.siyao@whamcloud.com>
2022-01-09 11:22:32 -0500

    lustre: lmv: improve MDT QOS space balance
    When MDTs are not balanced, QOS code tries to keep subdirectory
    creation local to the same MDT when it is deep in the directory
    tree, to avoid creating too many remote directories, but the
    existing weight to stay on the parent MDT until 50% of other MDTs
    is too radical, and causes mkdirs to be "stuck" on the same MDT.
    
    * remove "lq_threshold_rr" from above calculation because the check
      in ltd_qos_is_usable() handles this, so use only "dir_depth".
    * the factor is changed to "16 / (dir_depth + 10)", then it's less
      likely to stick to the parent MDT for top levels, while more
      likely to stay on the parent MDT for low levels:
      depth=0 -> 160%, depth=4 -> 114%, depth=6 -> 100%,
      depth=8 -> 88%, depth=12 -> 72%
    * rename lli_depth to lli_dir_depth to make usage more clear.
    
    WC-bug-id: https://jira.whamcloud.com/browse/LU-15216
    Lustre-commit: 38c4c538f53fb5f0c ("LU-15216 lmv: improve MDT QOS space balance")
    Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/45544
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/llite/llite_internal.h
+++ fs/lustre/llite/llite_internal.h
@@ -184,7 +184,7 @@ struct ll_inode_info {
 			 */
 			pid_t				lli_opendir_pid;
 			/* directory depth to ROOT */
-			unsigned short			lli_depth;
+			unsigned short			lli_dir_depth;
 			/* stat will try to access statahead entries or start
 			 * statahead if this flag is set, and this flag will be
 			 * set upon dir open, and cleared when dir is closed,
