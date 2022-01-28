9ccdf675cf09b7cb09aa1d9d6ba1697610df5299
Lai Siyao <lai.siyao@whamcloud.com>
2022-01-28 14:56:10 -0500

    lustre: llite: deadlock in ll_new_node()
    ll_new_node() will call ll_dir_getstripe() to fetch parent default
    LMV if md_create() returns -EREMOTE, it should call
    ll_finish_md_op_data() before calling ll_dir_getstripe() because
    the latter will lock lli_lsm_sem again, which will deadlock.
    
    Fixes: 7578c2b576e9a80 ("lustre: ptlrpc: intent_getattr fetches default LMV")
    WC-bug-id: https://jira.whamcloud.com/browse/LU-15456
    Lustre-commit: 1ce2fee3156858e13 ("LU-15456 llite: deadlock in ll_new_node()")
    Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/46157
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>

--- fs/lustre/llite/namei.c
+++ fs/lustre/llite/namei.c
@@ -1643,11 +1643,11 @@ again:
 
 		ptlrpc_req_finished(request);
 		request = NULL;
+		ll_finish_md_op_data(op_data);
+		op_data = NULL;
 
 		err2 = ll_dir_getstripe(dir, (void **)&lum, &lumsize, &request,
 					OBD_MD_DEFAULT_MEA);
-		ll_finish_md_op_data(op_data);
-		op_data = NULL;
 		if (!err2) {
 			struct lustre_md md = { NULL };
 
