jasimmons1973/lustre

commit bf189988209d6f71a4c1fed0de4ee58ba822eba7

lustre: clio: turn on lockless for some kind of IO
We can safely turn on lockless for Direct IO
and no lock.

Direct IO will still enqueue lock in the server side,
and we could not use lockless for in the following case:

1) If group lock is held before DIO, use lockless will
make us deadlock, so we use group lock instead and trust
this to protect consistecy.

2) Direct IO might fallback to Buffer IO in some cases,
and we will restart Direct IO with normal lock holding

The main motivation for this patch is to support AIO.

WC-bug-id: https://jira.whamcloud.com/browse/LU-4198
Lustre-commit: 6bce536725ef ("LU-4198 clio: turn on lockless for some kind of IO")
Signed-off-by: Jinshan Xiong <jinshan.xiong@uber.com>
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/8201
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Patrick Farrell <farr0186@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/include/cl_object.h
@@ -1906,6 +1906,10 @@ struct cl_io {
 	 */
 				ci_async_readahead:1,
 	/**
+	 * Ignore lockless and do normal locking for this io.
+	 */
+				ci_ignore_lockless:1,
+	/**
 	 * Set if we've tried all mirrors for this read IO, if it's not set,
 	 * the read IO will check to-be-read OSCs' status, and make fast-switch
 	 * another mirror if some of the OSTs are not healthy.

fs/lustre/llite/vvp_io.c
@@ -538,6 +538,16 @@ static int vvp_io_rw_lock(const struct lu_env *env, struct cl_io *io,
 		ast_flags |= CEF_NONBLOCK;
 	if (io->ci_lock_no_expand)
 		ast_flags |= CEF_LOCK_NO_EXPAND;
+	if (vio->vui_fd) {
+		/* Group lock held means no lockless any more */
+		if (vio->vui_fd->fd_flags & LL_FILE_GROUP_LOCKED)
+			io->ci_ignore_lockless = 1;
+
+		if (ll_file_nolock(vio->vui_fd->fd_file) ||
+		    (vio->vui_fd->fd_flags & LL_FILE_LOCKLESS_IO &&
+		     !io->ci_ignore_lockless))
+			ast_flags |= CEF_NEVER;
+	}
 
 	result = vvp_mmap_locks(env, vio, io);
 	if (result == 0)

fs/lustre/include/lustre_osc.h
@@ -936,7 +936,9 @@ struct osc_extent {
 	 */
 				oe_urgent:1,
 	/* Non-delay RPC should be used for this extent. */
-				oe_ndelay:1;
+				oe_ndelay:1,
+	/* direct IO pages */
+				oe_dio:1;
 	/* how many grants allocated for this extent.
 	 *  Grant allocated for this extent. There is no grant allocated
 	 *  for reading extents and sync write extents.

fs/lustre/llite/file.c
@@ -876,6 +876,10 @@ restart:
 		goto out_och_free;
 
 	mutex_unlock(&lli->lli_och_mutex);
+
+	/* lockless for direct IO so that it can do IO in parallel */
+	if (file->f_flags & O_DIRECT)
+		fd->fd_flags |= LL_FILE_LOCKLESS_IO;
 	fd = NULL;
 
 	/* Must do this outside lli_och_mutex lock to prevent deadlock where
@@ -886,8 +890,6 @@ restart:
 		goto out_och_free;
 
 	cl_lov_delay_create_clear(&file->f_flags);
-	goto out_och_free;
-
 out_och_free:
 	if (rc) {
 		if (och_p && *och_p) {
@@ -1503,6 +1505,7 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 	ssize_t result = 0;
 	int rc = 0;
 	unsigned int retried = 0;
+	unsigned int ignore_lockless = 0;
 
 	CDEBUG(D_VFSTRACE, "file: %pD, type: %d ppos: %llu, count: %zu\n",
 	       file, iot, *ppos, count);
@@ -1510,6 +1513,7 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 restart:
 	io = vvp_env_thread_io(env);
 	ll_io_init(io, file, iot == CIT_WRITE, args);
+	io->ci_ignore_lockless = ignore_lockless;
 	io->ci_ndelay_tried = retried;
 
 	if (cl_io_rw_init(env, io, iot, *ppos, count) == 0) {
@@ -1572,7 +1576,8 @@ out:
 	       file->f_path.dentry->d_name.name,
 	       iot, rc, result, io->ci_need_restart);
 
-	if ((!rc || rc == -ENODATA) && count > 0 && io->ci_need_restart) {
+	if ((!rc || rc == -ENODATA || rc == -ENOLCK) &&
+	    count > 0 && io->ci_need_restart) {
 		CDEBUG(D_VFSTRACE,
 		       "%s: restart %s from %lld, count:%zu, result: %zd\n",
 		       file_dentry(file)->d_name.name,
@@ -1580,6 +1585,7 @@ out:
 		       *ppos, count, result);
 		/* preserve the tried count for FLR */
 		retried = io->ci_ndelay_tried;
+		ignore_lockless = io->ci_ignore_lockless;
 		goto restart;
 	}
 

fs/lustre/llite/rw.c
@@ -1580,6 +1580,20 @@ int ll_readpage(struct file *file, struct page *vmpage)
 		return result;
 	}
 
+	/**
+	 * Direct read can fall back to buffered read, but DIO is done
+	 * with lockless i/o, and buffered requires LDLM locking, so in
+	 * this case we must restart without lockless.
+	 */
+	if (file->f_flags & O_DIRECT &&
+	    lcc && lcc->lcc_type == LCC_RW &&
+	    !io->ci_ignore_lockless) {
+		unlock_page(vmpage);
+		io->ci_ignore_lockless = 1;
+		io->ci_need_restart = 1;
+		return -ENOLCK;
+	}
+
 	page = cl_page_find(env, clob, vmpage->index, vmpage, CPT_CACHEABLE);
 	if (!IS_ERR(page)) {
 		LASSERT(page->cp_type == CPT_CACHEABLE);

fs/lustre/llite/rw26.c
@@ -476,16 +476,29 @@ static int ll_write_begin(struct file *file, struct address_space *mapping,
 	env = lcc->lcc_env;
 	io  = lcc->lcc_io;
 
-	if (file->f_flags & O_DIRECT && io->ci_designated_mirror > 0) {
+	if (file->f_flags & O_DIRECT) {
 		/* direct IO failed because it couldn't clean up cached pages,
 		 * this causes a problem for mirror write because the cached
 		 * page may belong to another mirror, which will result in
 		 * problem submitting the I/O.
 		 */
-		result = -EBUSY;
-		goto out;
-	}
+		if (io->ci_designated_mirror > 0) {
+			result = -EBUSY;
+			goto out;
+		}
 
+		/*
+		 * Direct read can fall back to buffered read, but DIO is done
+		 * with lockless i/o, and buffered requires LDLM locking, so
+		 * in this case we must restart without lockless.
+		 */
+		if (!io->ci_ignore_lockless) {
+			io->ci_ignore_lockless = 1;
+			io->ci_need_restart = 1;
+			result = -ENOLCK;
+			goto out;
+		}
+	}
 again:
 	/* To avoid deadlock, try to lock page first. */
 	vmpage = grab_cache_page_nowait(mapping, index);

fs/lustre/llite/vvp_page.c
@@ -412,39 +412,6 @@ static const struct cl_page_operations vvp_page_ops = {
 	},
 };
 
-static int vvp_transient_page_prep(const struct lu_env *env,
-				   const struct cl_page_slice *slice,
-				   struct cl_io *unused)
-{
-	/* transient page should always be sent. */
-	return 0;
-}
-
-static int vvp_transient_page_own(const struct lu_env *env,
-				  const struct cl_page_slice *slice,
-				  struct cl_io *unused, int nonblock)
-{
-	return 0;
-}
-
-static void vvp_transient_page_assume(const struct lu_env *env,
-				      const struct cl_page_slice *slice,
-				      struct cl_io *unused)
-{
-}
-
-static void vvp_transient_page_unassume(const struct lu_env *env,
-					const struct cl_page_slice *slice,
-					struct cl_io *unused)
-{
-}
-
-static void vvp_transient_page_disown(const struct lu_env *env,
-				      const struct cl_page_slice *slice,
-				      struct cl_io *unused)
-{
-}
-
 static void vvp_transient_page_discard(const struct lu_env *env,
 				       const struct cl_page_slice *slice,
 				       struct cl_io *unused)
@@ -460,20 +427,7 @@ static void vvp_transient_page_discard(const struct lu_env *env,
 static int vvp_transient_page_is_vmlocked(const struct lu_env *env,
 					  const struct cl_page_slice *slice)
 {
-	struct inode *inode = vvp_object_inode(slice->cpl_obj);
-	int locked;
-
-	locked = !inode_trylock(inode);
-	if (!locked)
-		inode_unlock(inode);
-	return locked ? -EBUSY : -ENODATA;
-}
-
-static void
-vvp_transient_page_completion(const struct lu_env *env,
-			      const struct cl_page_slice *slice,
-			      int ioret)
-{
+	return -EBUSY;
 }
 
 static void vvp_transient_page_fini(const struct lu_env *env,
@@ -481,32 +435,17 @@ static void vvp_transient_page_fini(const struct lu_env *env,
 				    struct pagevec *pvec)
 {
 	struct vvp_page *vpg = cl2vvp_page(slice);
-	struct cl_page *clp = slice->cpl_page;
-	struct vvp_object *clobj = cl2vvp(clp->cp_obj);
+	struct vvp_object *clobj = cl2vvp(slice->cpl_obj);
 
 	vvp_page_fini_common(vpg, pvec);
 	atomic_dec(&clobj->vob_transient_pages);
 }
 
 static const struct cl_page_operations vvp_transient_page_ops = {
-	.cpo_own		= vvp_transient_page_own,
-	.cpo_assume		= vvp_transient_page_assume,
-	.cpo_unassume		= vvp_transient_page_unassume,
-	.cpo_disown		= vvp_transient_page_disown,
 	.cpo_discard		= vvp_transient_page_discard,
 	.cpo_fini		= vvp_transient_page_fini,
 	.cpo_is_vmlocked	= vvp_transient_page_is_vmlocked,
 	.cpo_print		= vvp_page_print,
-	.io = {
-		[CRT_READ] = {
-			.cpo_prep	= vvp_transient_page_prep,
-			.cpo_completion = vvp_transient_page_completion,
-		},
-		[CRT_WRITE] = {
-			.cpo_prep	= vvp_transient_page_prep,
-			.cpo_completion	= vvp_transient_page_completion,
-		}
-	}
 };
 
 int vvp_page_init(const struct lu_env *env, struct cl_object *obj,

fs/lustre/osc/osc_cache.c
@@ -322,7 +322,8 @@ out:
 	__res;								\
 })
 
-static inline int overlapped(struct osc_extent *ex1, struct osc_extent *ex2)
+static inline bool
+overlapped(const struct osc_extent *ex1, const struct osc_extent *ex2)
 {
 	return !(ex1->oe_end < ex2->oe_start || ex2->oe_end < ex1->oe_start);
 }
@@ -879,6 +880,7 @@ int osc_extent_finish(const struct lu_env *env, struct osc_extent *ext,
 {
 	struct client_obd *cli = osc_cli(ext->oe_obj);
 	struct osc_async_page *oap;
+	struct osc_async_page *tmp;
 	int nr_pages = ext->oe_nr_pages;
 	int lost_grant = 0;
 	int blocksize = cli->cl_import->imp_obd->obd_osfs.os_bsize ? : 4096;
@@ -891,9 +893,8 @@ int osc_extent_finish(const struct lu_env *env, struct osc_extent *ext,
 	EASSERT(ergo(rc == 0, ext->oe_state == OES_RPC), ext);
 
 	osc_lru_add_batch(cli, &ext->oe_pages);
-	while ((oap = list_first_entry_or_null(&ext->oe_pages,
-					       struct osc_async_page,
-					       oap_pending_item))) {
+	list_for_each_entry_safe(oap, tmp, &ext->oe_pages,
+				 oap_pending_item) {
 		list_del_init(&oap->oap_rpc_item);
 		list_del_init(&oap->oap_pending_item);
 		if (last_off <= oap->oap_obj_off) {
@@ -1884,6 +1885,31 @@ static inline unsigned int osc_extent_chunks(const struct osc_extent *ext)
 	return (ext->oe_end >> ppc_bits) - (ext->oe_start >> ppc_bits) + 1;
 }
 
+static inline bool
+can_merge(const struct osc_extent *ext, const struct osc_extent *in_rpc)
+{
+	if (ext->oe_no_merge || in_rpc->oe_no_merge)
+		return false;
+
+	if (ext->oe_srvlock != in_rpc->oe_srvlock)
+		return false;
+
+	if (ext->oe_ndelay != in_rpc->oe_ndelay)
+		return false;
+
+	if (!ext->oe_grants != !in_rpc->oe_grants)
+		return false;
+
+	if (ext->oe_dio != in_rpc->oe_dio)
+		return false;
+
+	/* It's possible to have overlap on DIO */
+	if (in_rpc->oe_dio && overlapped(ext, in_rpc))
+		return false;
+
+	return true;
+}
+
 /**
  * Try to add extent to one RPC. We need to think about the following things:
  * - # of pages must not be over max_pages_per_rpc
@@ -1895,9 +1921,6 @@ static int try_to_add_extent_for_io(struct client_obd *cli,
 {
 	struct osc_extent *tmp;
 	unsigned int chunk_count;
-	struct osc_async_page *oap = list_first_entry(&ext->oe_pages,
-						      struct osc_async_page,
-						      oap_pending_item);
 
 	EASSERT((ext->oe_state == OES_CACHE || ext->oe_state == OES_LOCK_DONE),
 		ext);
@@ -1924,21 +1947,9 @@ static int try_to_add_extent_for_io(struct client_obd *cli,
 		return 0;
 
 	list_for_each_entry(tmp, data->erd_rpc_list, oe_link) {
-		struct osc_async_page *oap2;
-
-		oap2 = list_first_entry(&tmp->oe_pages, struct osc_async_page,
-					oap_pending_item);
 		EASSERT(tmp->oe_owner == current, tmp);
-		if (oap2cl_page(oap)->cp_type != oap2cl_page(oap2)->cp_type) {
-			CDEBUG(D_CACHE,
-			       "Do not permit different type of IO in one RPC\n");
-			return 0;
-		}
 
-		if (tmp->oe_srvlock != ext->oe_srvlock ||
-		    !tmp->oe_grants != !ext->oe_grants ||
-		    tmp->oe_ndelay != ext->oe_ndelay ||
-		    tmp->oe_no_merge || ext->oe_no_merge)
+		if (!can_merge(ext, tmp))
 			return 0;
 
 		/* remove break for strict check */
@@ -2654,6 +2665,7 @@ int osc_queue_sync_pages(const struct lu_env *env, const struct cl_io *io,
 	ext->oe_obj = obj;
 	ext->oe_srvlock = !!(brw_flags & OBD_BRW_SRVLOCK);
 	ext->oe_ndelay = !!(brw_flags & OBD_BRW_NDELAY);
+	ext->oe_dio = !!(brw_flags & OBD_BRW_NOCACHE);
 	ext->oe_nr_pages = page_count;
 	ext->oe_mppr = mppr;
 	list_splice_init(list, &ext->oe_pages);

commit 39bb129cf2dd5c7a4eb5e4e5ef7799f3453c75c6

lustre: llite: don't use a kms if it invalid.
Lockless DIO don't update a KMS as other IO type does,
it caused a situation when next read don't known a real file size
to be read. Lets avoid using an invalid KMS.

Fixes: bf18998820 ("lustre: clio: turn on lockless for some kind of IO")
WC-bug-id: https://jira.whamcloud.com/browse/LU-15829
Lustre-commit: dc907414db16d99e7 ("LU-15829 llite: don't use a kms if it invalid.")
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-on: https://review.whamcloud.com/47395
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>

fs/lustre/include/cl_object.h
@@ -138,6 +138,8 @@ struct cl_device {
 struct cl_attr {
 	/** Object size, in bytes */
 	loff_t		cat_size;
+
+	unsigned int cat_kms_valid:1;
 	/**
 	 * Known minimal size, in bytes.
 	 *

fs/lustre/llite/vvp_io.c
@@ -151,7 +151,7 @@ static int vvp_prep_size(const struct lu_env *env, struct cl_object *obj,
 	result = cl_object_attr_get(env, obj, attr);
 	if (result == 0) {
 		kms = attr->cat_kms;
-		if (pos > kms) {
+		if (pos > kms || !attr->cat_kms_valid) {
 			/*
 			 * A glimpse is necessary to determine whether we
 			 * return a short read (B) or some zeroes at the end

fs/lustre/lov/lov_internal.h
@@ -271,7 +271,7 @@ extern struct lu_kmem_descr lov_caches[];
 
 /* lov_merge.c */
 int lov_merge_lvb_kms(struct lov_stripe_md *lsm, int index,
-		      struct ost_lvb *lvb, u64 *kms_place);
+		      struct cl_attr *attr);
 
 /* lov_offset.c */
 u64 stripe_width(struct lov_stripe_md *lsm, unsigned int index);

fs/lustre/lov/lov_merge.c
@@ -32,6 +32,7 @@
 
 #define DEBUG_SUBSYSTEM S_LOV
 
+#include <cl_object.h>
 #include <obd_class.h>
 #include "lov_internal.h"
 
@@ -41,25 +42,23 @@
  * uptodate time on the local client.
  */
 int lov_merge_lvb_kms(struct lov_stripe_md *lsm, int index,
-		      struct ost_lvb *lvb, u64 *kms_place)
+		      struct cl_attr *attr)
 {
 	struct lov_stripe_md_entry *lse = lsm->lsm_entries[index];
 	u64 size = 0;
 	u64 kms = 0;
 	u64 blocks = 0;
-	s64 current_mtime = lvb->lvb_mtime;
-	s64 current_atime = lvb->lvb_atime;
-	s64 current_ctime = lvb->lvb_ctime;
+	/* XXX: timestamps can be negative by sanity:test_39m,
+	 * how can it be?
+	 */
+	s64 current_mtime = LLONG_MIN;
+	s64 current_atime = LLONG_MIN;
+	s64 current_ctime = LLONG_MIN;
 	int i;
 	int rc = 0;
 
 	assert_spin_locked(&lsm->lsm_lock);
 	LASSERT(lsm->lsm_lock_owner == current->pid);
-
-	CDEBUG(D_INODE,
-	       "MDT ID " DOSTID " initial value: s=%llu m=%llu a=%llu c=%llu b=%llu\n",
-	       POSTID(&lsm->lsm_oi), lvb->lvb_size, lvb->lvb_mtime,
-	       lvb->lvb_atime, lvb->lvb_ctime, lvb->lvb_blocks);
 
 	for (i = 0; i < lse->lsme_stripe_count; i++) {
 		struct lov_oinfo *loi = lse->lsme_oinfo[i];
@@ -70,7 +69,12 @@ int lov_merge_lvb_kms(struct lov_stripe_md *lsm, int index,
 			continue;
 		}
 
-		tmpsize = loi->loi_kms;
+		if (loi->loi_kms_valid) {
+			attr->cat_kms_valid = 1;
+			tmpsize = loi->loi_kms;
+		} else {
+			tmpsize = 0;
+		}
 		lov_size = lov_stripe_size(lsm, index, tmpsize, i);
 		if (lov_size > kms)
 			kms = lov_size;
@@ -91,18 +95,20 @@ int lov_merge_lvb_kms(struct lov_stripe_md *lsm, int index,
 			current_ctime = loi->loi_lvb.lvb_ctime;
 
 		CDEBUG(D_INODE,
-		       "MDT ID " DOSTID " on OST[%u]: s=%llu m=%llu a=%llu c=%llu b=%llu\n",
+		       "MDT ID " DOSTID " on OST[%u]: s=%llu (%d) m=%llu a=%llu c=%llu b=%llu\n",
 		       POSTID(&lsm->lsm_oi), loi->loi_ost_idx,
-		       loi->loi_lvb.lvb_size, loi->loi_lvb.lvb_mtime,
-		       loi->loi_lvb.lvb_atime, loi->loi_lvb.lvb_ctime,
-		       loi->loi_lvb.lvb_blocks);
+		       loi->loi_lvb.lvb_size, loi->loi_kms_valid,
+		       loi->loi_lvb.lvb_mtime, loi->loi_lvb.lvb_atime,
+		       loi->loi_lvb.lvb_ctime, loi->loi_lvb.lvb_blocks);
 	}
 
-	*kms_place = kms;
-	lvb->lvb_size = size;
-	lvb->lvb_blocks = blocks;
-	lvb->lvb_mtime = current_mtime;
-	lvb->lvb_atime = current_atime;
-	lvb->lvb_ctime = current_ctime;
+	if (!rc) {
+		attr->cat_kms = kms;
+		attr->cat_size = size;
+		attr->cat_mtime = current_mtime;
+		attr->cat_atime = current_atime;
+		attr->cat_ctime = current_ctime;
+		attr->cat_blocks = blocks;
+	}
 	return rc;
 }

fs/lustre/lov/lov_object.c
@@ -373,30 +373,13 @@ static int lov_attr_get_raid0(const struct lu_env *env, struct lov_object *lov,
 {
 	struct lov_layout_raid0 *r0 = &lle->lle_raid0;
 	struct lov_stripe_md *lsm = lov->lo_lsm;
-	struct ost_lvb *lvb = &lov_env_info(env)->lti_lvb;
 	struct cl_attr *attr = &r0->lo_attr;
-	u64 kms = 0;
 	int result = 0;
 
 	if (r0->lo_attr_valid) {
 		*lov_attr = attr;
 		return 0;
 	}
-
-	memset(lvb, 0, sizeof(*lvb));
-
-	/* XXX: timestamps can be negative by sanity:test_39m,
-	 * how can it be?
-	 */
-	lvb->lvb_atime = LLONG_MIN;
-	lvb->lvb_ctime = LLONG_MIN;
-	lvb->lvb_mtime = LLONG_MIN;
-
-	/*
-	 * XXX that should be replaced with a loop over sub-objects,
-	 * doing cl_object_attr_get() on them. But for now, let's
-	 * reuse old lov code.
-	 */
 
 	/*
 	 * XXX take lsm spin-lock to keep lov_merge_lvb_kms()
@@ -405,11 +388,9 @@ static int lov_attr_get_raid0(const struct lu_env *env, struct lov_object *lov,
 	 * sub-object attributes.
 	 */
 	lov_stripe_lock(lsm);
-	result = lov_merge_lvb_kms(lsm, index, lvb, &kms);
+	result = lov_merge_lvb_kms(lsm, index, attr);
 	lov_stripe_unlock(lsm);
 	if (result == 0) {
-		cl_lvb2attr(attr, lvb);
-		attr->cat_kms = kms;
 		r0->lo_attr_valid = 1;
 		*lov_attr = attr;
 	}
@@ -1042,6 +1023,8 @@ static int lov_attr_get_composite(const struct lu_env *env,
 		       lov_attr->cat_ctime, lov_attr->cat_blocks);
 
 		/* merge results */
+		if (lov_attr->cat_kms_valid)
+			attr->cat_kms_valid = 1;
 		attr->cat_blocks += lov_attr->cat_blocks;
 		if (attr->cat_size < lov_attr->cat_size)
 			attr->cat_size = lov_attr->cat_size;